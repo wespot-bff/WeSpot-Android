2b85b8f0bc724c37c44c898c12c620b799a8d0af
jeongjaino <akgk155@gmail.com>
2024-06-13 23:37:59 +0900

    [CHORE]#5 : detekt 종속성 및 플러그인 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,25 +3,11 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.detekt) apply false
+    alias(libs.plugins.ktlint) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.firebase.crashlytics) apply false
-    alias(libs.plugins.ktlint)
-}
-
-allprojects {
-    apply {
-        plugin(rootProject.libs.plugins.ktlint.get().pluginId)
-    }
-
-    ktlint {
-        android.set(true)
-        verbose.set(true)
-        outputToConsole.set(true)
-        reporters {
-            reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.PLAIN)
-        }
-    }
 }
