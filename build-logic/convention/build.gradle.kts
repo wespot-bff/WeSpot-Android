9e4aa3cc722a17878d6bd5233c6d1fbeda8ab450
jeongjaino <akgk155@gmail.com>
2024-06-15 22:39:01 +0900

    [CHORE]#2 : JVM Plugin 생성

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -20,16 +20,16 @@ gradlePlugin {
             implementationClass = "com.bff.wespot.plugin.AndroidApplicationPlugin"
         }
         create("androidLibrary") {
-            id = "com.bff.wespot.library"
+            id = "com.bff.wespot.android.library"
             implementationClass = "com.bff.wespot.plugin.AndroidLibraryPlugin"
-        }
-        create("androidFirebase") {
-            id = "com.bff.wespot.firebase"
-            implementationClass = "com.bff.wespot.plugin.AndroidApplicationFirebasePlugin"
         }
         create("androidFeature") {
             id = "com.bff.wespot.feature"
             implementationClass = "com.bff.wespot.plugin.AndroidFeaturePlugin"
+        }
+        create("androidFirebase") {
+            id = "com.bff.wespot.firebase"
+            implementationClass = "com.bff.wespot.plugin.AndroidApplicationFirebasePlugin"
         }
         create("androidCompose") {
             id = "com.bff.wespot.compose"
@@ -38,6 +38,10 @@ gradlePlugin {
         create("androidHilt") {
             id = "com.bff.wespot.hilt"
             implementationClass = "com.bff.wespot.plugin.AndroidHiltPlugin"
+        }
+        create("jvmLibrary") {
+            id = "com.bff.wespot.jvm.library"
+            implementationClass = "com.bff.wespot.plugin.JvmLibraryPlugin"
         }
     }
 }
