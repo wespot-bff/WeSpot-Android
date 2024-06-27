8074b0675ef5e549de6619ca399364a541209995
flash159483 <seungwonjung0313@gmail.com>
2024-06-27 17:10:54 +0900

    [CHORE]#3: Theme 추가

---
+++ core/ui/src/main/kotlin/com/bff/wespot/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.bff.wespot.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(12.dp),
+    medium = RoundedCornerShape(16.dp),
+    large = RoundedCornerShape(4.dp)
+)
