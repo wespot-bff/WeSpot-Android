7227cd08b17e13127568ee4bee5e53085611638e
flash159483 <seungwonjung0313@gmail.com>
2024-07-04 20:48:13 +0900

    [FEAT]#12: WSDialog 추가

--- designsystem/src/main/kotlin/com/bff/wespot/designsystem/component/button/WSButton.kt
+++ designsystem/src/main/kotlin/com/bff/wespot/designsystem/component/button/WSButton.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -36,6 +37,7 @@ fun WSButton(
     onClick: () -> Unit,
     text: String = "",
     buttonType: WSButtonType = WSButtonType.Primary,
+    paddingValues: PaddingValues = PaddingValues(vertical = 12.dp, horizontal = 20.dp),
     enabled: Boolean = true,
     borderStroke: BorderStroke? = null,
     pressedBorderStroke: BorderStroke? = null,
@@ -49,10 +51,9 @@ fun WSButton(
     val isPressed by interactionSource.collectIsPressedAsState()
 
     Box(
-        modifier =
-        Modifier
+        modifier = Modifier
             .wrapContentSize()
-            .padding(vertical = 12.dp, horizontal = 20.dp),
+            .padding(paddingValues),
     ) {
         Button(
             modifier = Modifier.fillMaxWidth(),
@@ -77,12 +78,13 @@ fun WSButton(
             } else {
                 borderStroke
             },
+            contentPadding = PaddingValues(0.dp)
         ) {
             content {
                 Text(
                     text = text,
                     style = buttonType.fontStyle(),
-                    modifier = Modifier.padding(vertical = 16.dp),
+                    modifier = Modifier.padding(vertical = 14.dp),
                 )
             }
         }
