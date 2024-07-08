9c88252496e60166eca2c22ff34ee80820a7d442
jeongjaino <akgk155@gmail.com>
2024-07-09 01:35:24 +0900

    [CHORE]#14 : Ktor Client 객체 및 주입 구현

---
+++ core/network/src/main/kotlin/com/bff/wespot/network/di/ClientModule.kt
@@ -0,0 +1,80 @@
+package com.bff.wespot.network.di
+
+import android.util.Log
+import com.bff.wespot.network.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.auth.providers.bearer
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ClientModule {
+    private const val REQUEST_TIMEOUT_MILLIS = 50_000L
+    private const val CLIENT_TAG = "HttpClient"
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(
+        ktorJsonSetting: Json,
+    ): HttpClient = HttpClient(CIO) {
+        defaultRequest {
+            url(BuildConfig.MOCK_BASE_URL)
+        }
+
+        install(Auth) {
+            bearer {
+                /*refreshTokens {
+                    val token = client.get {
+                        markAsRefreshTokenRequest()
+                        url("refreshToken")
+                        parameter("refreshToken", localService.getRefreshToken())
+                    }.body<Token>()
+                    BearerTokens(
+                        accessToken = token.bearerToken,
+                        refreshToken = token.refreshToken
+                    )
+                }*/
+            }
+        }
+
+        install(HttpTimeout) {
+            requestTimeoutMillis = REQUEST_TIMEOUT_MILLIS
+        }
+
+        install(ContentNegotiation) {
+            json(
+                json = Json {
+                    encodeDefaults = true // Default 값 포함한 직렬화 수행
+                    isLenient = true // 비표준 Json 형식 허용
+                    prettyPrint = true
+                    ignoreUnknownKeys = true
+                }
+            )
+        }
+
+        install(Logging) {
+            level = LogLevel.ALL
+            logger = object : Logger {
+                override fun log(message: String) {
+                    if (BuildConfig.DEBUG) {
+                        Log.i(CLIENT_TAG, message)
+                    }
+                }
+            }
+        }
+    }
+}
