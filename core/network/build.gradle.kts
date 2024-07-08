f4548ec0bb9db73adc6392706202c50e69bc9d93
jeongjaino <akgk155@gmail.com>
2024-07-08 23:57:44 +0900

    [CHORE]#14 : Network 모듈 생성 및 Serialization 설정

---
+++ core/network/build.gradle.kts
@@ -0,0 +1,30 @@
+import org.jetbrains.kotlin.konan.properties.Properties
+
+val properties = Properties()
+properties.load(rootProject.file("local.properties").inputStream())
+
+plugins {
+    alias(libs.plugins.wespot.android.library)
+    alias(libs.plugins.wespot.android.hilt)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+
+android {
+    namespace = "com.bff.wespot.network"
+
+    defaultConfig {
+        buildConfigField("String", "MOCK_BASE_URL", properties.getProperty("MOCK_BASE_URL"))
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+dependencies {
+    implementation(libs.junit)
+    implementation(libs.bundles.ktor.client)
+    implementation(libs.kotlin.serialization.json)
+    implementation(project(":core:model"))
+}
