eab1fbf0bdbff4b18324d9fa4fc026c4221014c6
jeongjaino <akgk155@gmail.com>
2024-07-10 00:38:52 +0900

    [FEATURE]#14 : 쪽지 리스트 불러오기 기능 구현 UseCase

---
+++ domain/src/main/kotlin/com/bff/wespot/domain/usecase/message/GetMessageListUseCase.kt
@@ -0,0 +1,16 @@
+package com.bff.wespot.domain.usecase.message
+
+import com.bff.wespot.domain.repository.message.MessageRepository
+import com.bff.wespot.model.message.MessageList
+import com.bff.wespot.model.message.MessageType
+import com.bff.wespot.model.result.NetworkError
+import com.bff.wespot.model.result.Result
+import javax.inject.Inject
+
+class GetMessageListUseCase @Inject constructor(
+    private val messageRepository: MessageRepository,
+) {
+    suspend operator fun invoke(messageType: MessageType): Result<MessageList, NetworkError> {
+        return messageRepository.getMessageList(messageType)
+    }
+}
