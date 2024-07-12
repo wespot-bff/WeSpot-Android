5cf76901d70757d0fd8d08160e6d05e62ea36691
정진호 <akgk155@gmail.com>
2024-07-12 13:44:02 +0900

    [CHORE]#14 : HttpClient Extension으로 변경

--- core/network/src/main/kotlin/com/bff/wespot/network/source/message/MessageDataSourceImpl.kt
+++ core/network/src/main/kotlin/com/bff/wespot/network/source/message/MessageDataSourceImpl.kt
@@ -2,46 +2,49 @@ package com.bff.wespot.network.source.message
 
 import com.bff.wespot.network.model.result.ErrorDto
 import com.bff.wespot.model.result.Result
+import com.bff.wespot.network.extensions.safeRequest
 import com.bff.wespot.network.model.message.request.MessageTypeDto
 import com.bff.wespot.network.model.message.request.SentMessageDto
 import com.bff.wespot.network.model.message.request.name
 import com.bff.wespot.network.model.message.response.MessageIdDto
-import com.bff.wespot.network.model.result.parseResponse
 import com.bff.wespot.network.model.message.response.MessageListDto
 import com.bff.wespot.network.model.message.response.MessageStatusDto
 import io.ktor.client.HttpClient
-import io.ktor.client.request.get
 import io.ktor.client.request.parameter
-import io.ktor.client.request.post
 import io.ktor.client.request.setBody
+import io.ktor.http.*
 import javax.inject.Inject
 
 class MessageDataSourceImpl @Inject constructor(
-    private val client: HttpClient,
+    private val httpClient: HttpClient,
 ): MessageDataSource {
     override suspend fun getMessageList(
         messageTypeDto: MessageTypeDto,
-    ): Result<MessageListDto, ErrorDto> {
-        return parseResponse {
-            client.get("messages") {
+    ): Result<MessageListDto, ErrorDto> =
+        httpClient.safeRequest {
+            url {
+                method = HttpMethod.Get
+                path("messages")
                 parameter("type", messageTypeDto.name())
             }
         }
-    }
 
     override suspend fun postMessage(
         sentMessageDto: SentMessageDto,
-    ): Result<MessageIdDto, ErrorDto> {
-        return parseResponse {
-            client.post("messages/send") {
+    ): Result<MessageIdDto, ErrorDto> =
+        httpClient.safeRequest {
+            url {
+                method = HttpMethod.Post
+                path("messages/send")
                 setBody(sentMessageDto)
             }
         }
-    }
 
-    override suspend fun getMessageStatus(): Result<MessageStatusDto, ErrorDto> {
-        return parseResponse {
-            client.get("messages/status/me")
+    override suspend fun getMessageStatus(): Result<MessageStatusDto, ErrorDto> =
+        httpClient.safeRequest {
+            url {
+                method = HttpMethod.Get
+                path("messages/status/me")
+            }
         }
     }
-}
