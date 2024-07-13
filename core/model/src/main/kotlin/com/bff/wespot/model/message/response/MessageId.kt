d282815ab5c9ed391b88210bfd82c2b93e5c0d34
jeongjaino <akgk155@gmail.com>
2024-07-14 01:34:09 +0900

    [CHORE]#14 : MessageId Domain Model 소거

--- core/model/src/main/kotlin/com/bff/wespot/model/message/response/MessageId.kt
+++
@@ -1,5 +0,0 @@
-package com.bff.wespot.model.message.response
-
-data class MessageId(
-    val id: String,
-)
--- core/network/src/main/kotlin/com/bff/wespot/network/model/message/response/MessageIdDto.kt
+++ core/network/src/main/kotlin/com/bff/wespot/network/model/message/response/MessageIdDto.kt
@@ -1,11 +1,8 @@
 package com.bff.wespot.network.model.message.response
 
-import com.bff.wespot.model.message.response.MessageId
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class MessageIdDto (
     val id: String,
-) {
-    fun toMessageId(): MessageId = MessageId(id)
-}
+)
