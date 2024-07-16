94f4b757d75d30ae97a7b17b0bc8b1ef51afa18b
jeongjaino <akgk155@gmail.com>
2024-07-17 01:10:40 +0900

    [FEATURE]#15 : MessageStatus 내부 비즈니스 로직 구현

--- core/model/src/main/kotlin/com/bff/wespot/model/message/response/MessageStatus.kt
+++ core/model/src/main/kotlin/com/bff/wespot/model/message/response/MessageStatus.kt
@@ -1,6 +1,18 @@
 package com.bff.wespot.model.message.response
 
 data class MessageStatus(
-    val canSend: Boolean,
-    val remainingMessages: Int,
-)
+    val canSend: Boolean = false,
+    val remainingMessages: Int = 0,
+) {
+    companion object {
+        private const val MESSAGE_COUNT_PER_DAY = 3
+    }
+
+    fun getReservedMessageCount(): Int = MESSAGE_COUNT_PER_DAY - remainingMessages
+
+    fun hasReservedMessages() = remainingMessages < MESSAGE_COUNT_PER_DAY
+
+    fun hasRemainingMessages() = remainingMessages > 0
+
+    fun hasNoRemainingMessages() = remainingMessages == 0
+}
