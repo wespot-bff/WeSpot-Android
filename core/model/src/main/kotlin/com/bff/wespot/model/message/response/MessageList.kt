32cd4e17cf8964f2db42f373d9005aec12fd9a2c
jeongjaino <akgk155@gmail.com>
2024-07-17 01:10:51 +0900

    [FEATURE]#15 : MessageList 내부 비즈니스 로직 구현

--- core/model/src/main/kotlin/com/bff/wespot/model/message/response/MessageList.kt
+++ core/model/src/main/kotlin/com/bff/wespot/model/message/response/MessageList.kt
@@ -1,6 +1,8 @@
 package com.bff.wespot.model.message.response
 
 data class MessageList(
-    val messages: List<Message>,
-    val isLastPage: Boolean,
-)
+    val messages: List<Message> = listOf(),
+    val isLastPage: Boolean = true,
+) {
+    fun hasUnReadMessages(): Boolean = messages.any { it.readAt == null }
+}
