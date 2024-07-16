e3a6e431d24332d3e47842aea45965da09384462
jeongjaino <akgk155@gmail.com>
2024-07-17 01:13:05 +0900

    [FEATURE]#15 : MessageActions 구현

---
+++ feature/message/src/main/kotlin/com/bff/wespot/message/state/MessageAction.kt
@@ -0,0 +1,11 @@
+package com.bff.wespot.message.state
+
+sealed class MessageAction {
+    data object OnHomeScreenEntered : MessageAction()
+    data class Navigation(val navigate: NavigationAction) : MessageAction()
+}
+
+sealed interface NavigationAction {
+    data object NavigateToSendScreen : NavigationAction
+    data object NavigateToStorageScreen : NavigationAction
+}
