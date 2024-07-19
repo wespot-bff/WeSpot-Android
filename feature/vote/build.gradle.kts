1b9e7788250c49a3f0130fd87e69bc2a8e193c80
flash159483 <seungwonjung0313@gmail.com>
2024-07-19 22:49:04 +0900

    [FEAT]#38: Vote Module 추가해요

---
+++ feature/vote/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    alias(libs.plugins.wespot.android.feature)
+    alias(libs.plugins.wespot.android.compose)
+    alias(libs.plugins.wespot.android.hilt)
+}
+
+android {
+    namespace = "com.bff.wespot.vote"
+}
+
+dependencies {
+    implementation(project(":domain"))
+    implementation(project(":core:model"))
+    implementation(project(":core:ui"))
+    implementation(project(":designsystem"))
+
+    implementation(libs.bundles.androidx.compose)
+    implementation(libs.bundles.orbit)
+    implementation(libs.junit)
+    implementation(libs.androidx.junit)
+}
