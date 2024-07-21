97a2ddc73b366f5d402dddf6210dd0879cb92a41
jeongjaino <akgk155@gmail.com>
2024-07-22 01:59:49 +0900

    [FEATURE]#17 : 랜덤 네이밍 익스텐션 구현

---
+++ feature/message/src/main/kotlin/com/bff/wespot/message/common/NameExtensions.kt
@@ -0,0 +1,17 @@
+package com.bff.wespot.message.common
+
+internal class NameExtensions {
+    fun invoke(): String = "${colorList.random()} ${animalList.random()}"
+
+    companion object {
+        private val colorList = listOf(
+            "빨간색", "주황색", "노란색", "초록색", "파란색", "남색", "보라색", "핑크색", "갈색", "연두색", "하늘색",
+        )
+
+        private val animalList = listOf(
+            "얼룩말",
+            "코알라",
+            "기린",
+        )
+    }
+}
