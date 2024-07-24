6ab6754a06d76ade543309b1d2895b0bb9a5cbcd
jeongjaino <akgk155@gmail.com>
2024-07-24 21:38:41 +0900

    [FEATURE]#17 : Navigiation Action 소거

--- feature/message/src/main/kotlin/com/bff/wespot/message/state/MessageAction.kt
+++ feature/message/src/main/kotlin/com/bff/wespot/message/state/MessageAction.kt
@@ -2,12 +2,4 @@ package com.bff.wespot.message.state
 
 sealed class MessageAction {
     data object OnHomeScreenEntered : MessageAction()
-    data class Navigation(val navigate: NavigationAction) : MessageAction()
-}
-
-sealed interface NavigationAction {
-    data object PopBackStack : NavigationAction
-    data class NavigateToReceiverScreen(val isEditing: Boolean) : NavigationAction
-    data object NavigateToStorageScreen : NavigationAction
-    data object NavigateToNotification : NavigationAction
 }
