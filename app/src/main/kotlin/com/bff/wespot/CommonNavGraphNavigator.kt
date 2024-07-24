baea6830d5bded6673c6b4f124e9269d8271ef66
jeongjaino <akgk155@gmail.com>
2024-07-24 21:42:26 +0900

    [FEATURE]#17 : 메시지 네비게이션 전환 구현

--- app/src/main/kotlin/com/bff/wespot/CommonNavGraphNavigator.kt
+++ app/src/main/kotlin/com/bff/wespot/CommonNavGraphNavigator.kt
@@ -3,6 +3,15 @@ package com.bff.wespot
 import androidx.navigation.NavController
 import com.bff.wespot.entire.screen.EntireNavigator
 import com.bff.wespot.message.screen.MessageNavigator
+import com.bff.wespot.message.screen.destinations.MessageEditScreenDestination
+import com.bff.wespot.message.screen.destinations.MessageScreenDestination
+import com.bff.wespot.message.screen.destinations.MessageWriteScreenDestination
+import com.bff.wespot.message.screen.destinations.ReceiverSelectionScreenDestination
+import com.bff.wespot.message.screen.send.MessageEditNavigator
+import com.bff.wespot.message.screen.send.MessageWriteNavigator
+import com.bff.wespot.message.screen.send.MessageWriteScreenArgs
+import com.bff.wespot.message.screen.send.ReceiverSelectionNavigator
+import com.bff.wespot.message.screen.send.ReceiverSelectionScreenArgs
 import com.bff.wespot.vote.screen.VoteNavigator
 import com.bff.wespot.vote.screen.VotingNavigator
 import com.bff.wespot.vote.screen.destinations.VotingScreenDestination
@@ -15,6 +24,9 @@ class CommonNavGraphNavigator(
     private val navController: NavController,
 ) : VoteNavigator,
     MessageNavigator,
+    ReceiverSelectionNavigator,
+    MessageWriteNavigator,
+    MessageEditNavigator,
     EntireNavigator,
     VotingNavigator {
     override fun navigateUp() {
@@ -23,5 +35,27 @@ class CommonNavGraphNavigator(
 
     override fun navigateToVotingScreen() {
         navController.navigate(VotingScreenDestination within navGraph)
+    }
+
+    override fun navigateReceiverSelectionScreen(args: ReceiverSelectionScreenArgs) {
+        navController.navigate(ReceiverSelectionScreenDestination(args) within navGraph)
+    }
+
+    override fun navigateMessageWriteScreen(args: MessageWriteScreenArgs) {
+        navController.navigate(MessageWriteScreenDestination(args) within navGraph)
+    }
+
+    override fun navigateMessageScreen() {
+        navController.navigate(MessageScreenDestination within navGraph)
+    }
+
+    override fun navigateMessageEditScreen() {
+        navController.navigate(MessageEditScreenDestination within navGraph)
+    }
+
+    override fun navigateStorageScreen() {
+    }
+
+    override fun navigateNotificationScreen() {
     }
 }
