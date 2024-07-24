fe31cafc81df4617f479bed09d2edad27874ffab
jeongjaino <akgk155@gmail.com>
2024-07-24 21:41:22 +0900

    [FEATURE]#17 : 메세지 수정 화면 구현

--- feature/message/src/main/kotlin/com/bff/wespot/message/screen/send/MessageEditScreen.kt
+++ feature/message/src/main/kotlin/com/bff/wespot/message/screen/send/MessageEditScreen.kt
@@ -14,52 +14,85 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.bff.wespot.designsystem.component.button.WSButton
 import com.bff.wespot.designsystem.component.button.WSButtonType
 import com.bff.wespot.designsystem.component.header.WSTopBar
-import com.bff.wespot.designsystem.component.input.WsTextField
-import com.bff.wespot.designsystem.component.input.WsTextFieldType
+import com.bff.wespot.designsystem.component.indicator.WSToast
+import com.bff.wespot.designsystem.component.indicator.WSToastType
 import com.bff.wespot.designsystem.component.modal.WSDialog
 import com.bff.wespot.designsystem.component.toggle.WSSwitch
 import com.bff.wespot.designsystem.theme.Gray400
 import com.bff.wespot.designsystem.theme.StaticTypeScale
 import com.bff.wespot.designsystem.theme.WeSpotThemeManager
-import com.bff.wespot.message.state.send.NavigationAction
+import com.bff.wespot.message.R
 import com.bff.wespot.message.state.send.SendAction
+import com.bff.wespot.message.state.send.SendSideEffect
 import com.bff.wespot.message.viewmodel.SendViewModel
 import com.bff.wespot.ui.LetterCountIndicator
+import com.bff.wespot.ui.MessageContentButton
+import com.ramcosta.composedestinations.annotation.Destination
 import org.orbitmvi.orbit.compose.collectAsState
+import org.orbitmvi.orbit.compose.collectSideEffect
 
+interface MessageEditNavigator {
+    fun navigateUp()
+    fun navigateReceiverSelectionScreen(args: ReceiverSelectionScreenArgs)
+    fun navigateMessageWriteScreen(args: MessageWriteScreenArgs)
+    fun navigateMessageScreen()
+}
+
+@Destination
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun EditScreen(
-    viewModel: SendViewModel = hiltViewModel(),
+fun MessageEditScreen(
+    navigator: MessageEditNavigator,
+    viewModel: SendViewModel,
 ) {
-    var dialogState by remember { mutableStateOf(false) }
-    var reserveDialogState by remember { mutableStateOf(false) }
+    var exitDialog by remember { mutableStateOf(false) }
+    var reserveDialog by remember { mutableStateOf(false) }
+    var timeoutDialog by remember { mutableStateOf(false) }
+    var toast by remember { mutableStateOf(false) }
 
     val state by viewModel.collectAsState()
     val action = viewModel::onAction
+
+    viewModel.collectSideEffect {
+        when (it) {
+            is SendSideEffect.ShowTimeoutDialog -> {
+                reserveDialog = false
+                timeoutDialog = true
+            }
+
+            is SendSideEffect.NavigateToMessage -> {
+                action(SendAction.NavigateToMessageHome)
+                navigator.navigateMessageScreen()
+            }
+        }
+    }
 
     Scaffold(
         topBar = {
             WSTopBar(
                 title = "",
                 canNavigateBack = true,
+                navigateUp = {
+                    navigator.navigateUp()
+                },
                 action = {
                     Text(
                         modifier = Modifier.clickable {
-                            dialogState = true
+                            exitDialog = true
                         },
-                        text = "닫기",
+                        text = stringResource(R.string.close),
                         style = StaticTypeScale.Default.body4,
                         color = WeSpotThemeManager.colors.abledTxtColor,
                     )
@@ -69,44 +102,31 @@ fun EditScreen(
     ) {
         Column(modifier = Modifier.padding(it)) {
             EditField(
-                title = "받는 사람",
-                value = requireNotNull(state.selectedUser).toDescription(),
+                title = stringResource(R.string.receiver),
+                value = state.selectedUser.toDescription(),
             ) {
-                action(
-                    SendAction.Navigation(
-                        NavigationAction.NavigateToReceiverScreen(true),
-                    ),
+                navigator.navigateReceiverSelectionScreen(
+                    args = ReceiverSelectionScreenArgs(isEditing = true),
                 )
             }
 
             Column {
                 Text(
-                    text = "전달할 내용",
+                    text = stringResource(R.string.message_sent_content),
                     style = StaticTypeScale.Default.body4,
                     modifier = Modifier.padding(top = 16.dp, start = 30.dp, end = 30.dp),
                 )
 
                 Spacer(modifier = Modifier.height(12.dp))
 
-                Box(
-                    modifier = Modifier
-                        .padding(horizontal = 20.dp)
-                        .clickable {
-                            action(
-                                SendAction.Navigation(
-                                    NavigationAction.NavigateToWriteScreen(true),
-                                ),
-                            )
-                        },
-                ) {
-                    WsTextField(
-                        value = state.messageInput,
-                        onValueChange = { },
-                        placeholder = "",
-                        isError = false,
-                        textFieldType = WsTextFieldType.Message,
-                    )
-                }
+                MessageContentButton(
+                    text = state.messageInput,
+                    onClick = {
+                        navigator.navigateMessageWriteScreen(
+                            args = MessageWriteScreenArgs(isEditing = true),
+                        )
+                    },
+                )
 
                 Box(
                     modifier = Modifier
@@ -119,15 +139,10 @@ fun EditScreen(
             }
 
             EditField(
-                title = "보내는 사람",
+                title = stringResource(R.string.sender),
                 value = if (state.isRandomName) state.randomName else state.profile.toDescription(),
-            ) {
-                action(
-                    SendAction.Navigation(
-                        NavigationAction.NavigateToReceiverScreen(true),
-                    ),
-                )
-            }
+                onClicked = { toast = true },
+            )
 
             Row(
                 modifier = Modifier
@@ -139,13 +154,13 @@ fun EditScreen(
                     verticalArrangement = Arrangement.spacedBy(6.dp),
                 ) {
                     Text(
-                        text = "랜덤 닉네임으로 보내기",
+                        text = stringResource(R.string.random_nickname_title),
                         style = StaticTypeScale.Default.body1,
                         color = WeSpotThemeManager.colors.txtTitleColor,
                     )
 
                     Text(
-                        text = "이 기능을 끄면 실명으로 전송돼요",
+                        text = stringResource(R.string.random_nickname_subtitle),
                         style = StaticTypeScale.Default.body8,
                         color = Gray400,
                     )
@@ -162,39 +177,70 @@ fun EditScreen(
         Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomCenter) {
             WSButton(
                 onClick = {
-                    reserveDialogState = true
+                    reserveDialog = true
                 },
-                text = "쪽지 보내기",
+                text = stringResource(R.string.message_send),
             ) {
                 it()
             }
         }
 
-        if (dialogState) {
+        if (exitDialog) {
             WSDialog(
-                title = "쪽지 작성을 중단하시나요",
-                subTitle = "작성 중인 내용은 저장되지 않아요",
-                okButtonText = "네 그만할래요",
-                cancelButtonText = "닫기",
+                title = stringResource(R.string.send_exit_dialog_title),
+                subTitle = stringResource(R.string.send_exit_dialog_subtitle),
+                okButtonText = stringResource(R.string.send_exit_dialog_ok_button),
+                cancelButtonText = stringResource(id = R.string.close),
                 okButtonClick = {
-                    action(SendAction.Navigation(NavigationAction.NavigateToMessageScreen))
+                    action(SendAction.NavigateToMessageHome)
+                    navigator.navigateMessageScreen()
                 },
-                cancelButtonClick = { dialogState = false },
-                onDismissRequest = { dialogState = false },
+                cancelButtonClick = { exitDialog = false },
+                onDismissRequest = { exitDialog = false },
             )
         }
 
-        if (reserveDialogState) {
+        if (reserveDialog) {
             WSDialog(
-                title = "쪽지를 예약할까요?",
-                subTitle = "오늘 밤 10시에 상대에게 쪽지를 전달해 드릴게요\n예약 이후 전송 취소는 어려워요",
-                okButtonText = "네 예약할래요",
-                cancelButtonText = "취소",
+                title = stringResource(R.string.message_send_dialog_title),
+                subTitle = stringResource(R.string.message_send_dialog_subtitle),
+                okButtonText = stringResource(R.string.message_send_dialog_button_text),
+                cancelButtonText = stringResource(R.string.cancel),
                 okButtonClick = { action(SendAction.SendMessage) },
-                cancelButtonClick = { reserveDialogState = false },
-                onDismissRequest = { reserveDialogState = false },
+                cancelButtonClick = { reserveDialog = false },
+                onDismissRequest = { reserveDialog = false },
             )
         }
+
+        if (timeoutDialog) {
+            WSDialog(
+                title = stringResource(R.string.timeout_dialog_title),
+                subTitle = stringResource(R.string.timeout_dialog_subtitle),
+                okButtonText = stringResource(R.string.positive_answer),
+                cancelButtonText = stringResource(R.string.close),
+                okButtonClick = {
+                    action(SendAction.NavigateToMessageHome)
+                    navigator.navigateMessageScreen()
+                },
+                cancelButtonClick = { timeoutDialog = false },
+                onDismissRequest = { timeoutDialog = false },
+            )
+        }
+    }
+
+    if (toast) {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopCenter) {
+            WSToast(
+                text = stringResource(R.string.toast_error_name_edit),
+                toastType = WSToastType.Error,
+                showToast = toast,
+                closeToast = { toast = false },
+            )
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        action(SendAction.OnEditScreenEntered)
     }
 }
 
