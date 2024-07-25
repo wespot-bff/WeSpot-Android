e9607f3cd70742c477331e0e5461624f8ed68ec1
정진호 <jeong.jinho@cashwalk.io>
2024-07-25 13:39:05 +0900

    [CHORE]#17 : 디벨롭 병합 수정

--- feature/vote/src/main/java/com/bff/wespot/vote/state/VoteAction.kt
+++
@@ -1,6 +0,0 @@
-package com.bff.wespot.vote.state
-
-sealed class VoteAction {
-    data object StartDate : VoteAction()
-    data object EndDate : VoteAction()
-}
--- feature/vote/src/main/java/com/bff/wespot/vote/state/VoteSideEffect.kt
+++
@@ -1,3 +0,0 @@
-package com.bff.wespot.vote.state
-
-sealed class VoteSideEffect
