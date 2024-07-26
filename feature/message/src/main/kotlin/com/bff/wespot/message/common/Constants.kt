c7e636319996acd18caa8dc873a7640fd2cfd6b0
정진호 <akgk155@gmail.com>
2024-07-26 18:00:58 +0900

    [FEATURE]#40 : 상수 및 stringResource 추가

--- feature/message/src/main/kotlin/com/bff/wespot/message/common/Constants.kt
+++ feature/message/src/main/kotlin/com/bff/wespot/message/common/Constants.kt
@@ -1,5 +1,7 @@
 package com.bff.wespot.message.common
 
 internal const val MESSAGE_MAX_LENGTH = 200
+internal const val HOME_SCREEN_INDEX = 0
+internal const val STORAGE_SCREEN_INDEX = 1
 internal const val SENT_MESSAGE_INDEX = 0
 internal const val RECEIVED_MESSAGE_INDEX = 1
