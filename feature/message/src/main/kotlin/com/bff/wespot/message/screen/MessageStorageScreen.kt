82df81a6586d5137f2dd475ccfc5461ba9e56cd4
jeongjaino <akgk155@gmail.com>
2024-07-27 15:59:48 +0900

    [FEATURE]#40 : 메세지 카드 높이 설정 및 x 버튼 영역 설정

--- feature/message/src/main/kotlin/com/bff/wespot/message/screen/MessageStorageScreen.kt
+++ feature/message/src/main/kotlin/com/bff/wespot/message/screen/MessageStorageScreen.kt
@@ -11,11 +11,14 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -27,12 +30,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.bff.wespot.designsystem.component.list.WSMessageItem
 import com.bff.wespot.designsystem.component.list.WSMessageItemType
 import com.bff.wespot.designsystem.theme.Primary400
@@ -47,13 +50,15 @@ import com.bff.wespot.message.model.TimePeriod
 import com.bff.wespot.message.state.MessageAction
 import com.bff.wespot.message.viewmodel.MessageViewModel
 import com.bff.wespot.model.message.request.MessageType
+import com.bff.wespot.ui.WSBottomSheet
 import com.bff.wespot.ui.WSHomeChipGroup
 import kotlinx.collections.immutable.persistentListOf
 import org.orbitmvi.orbit.compose.collectAsState
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MessageStorageScreen(
-    viewModel: MessageViewModel = hiltViewModel(),
+    viewModel: MessageViewModel,
     navigateToReceiverSelectionScreen: (Boolean) -> Unit,
 ) {
     val chipList = persistentListOf(
@@ -62,6 +67,7 @@ fun MessageStorageScreen(
     )
     var selectedChipIndex by remember { mutableIntStateOf(0) }
     var showMessageDialog by remember { mutableStateOf(false) }
+    var showBottomSheet by remember { mutableStateOf(false) }
 
     val state by viewModel.collectAsState()
     val action = viewModel::onAction
@@ -97,6 +103,7 @@ fun MessageStorageScreen(
                                 showMessageDialog = true
                             },
                             optionButtonClick = {
+                                showBottomSheet = true
                             },
                         )
                     }
@@ -124,10 +131,54 @@ fun MessageStorageScreen(
         }
     }
 
+    if (showBottomSheet) {
+        WSBottomSheet(
+            closeSheet = { showBottomSheet = false },
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(start = 20.dp, end = 20.dp, top = 12.dp, bottom = 32.dp),
+            ) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    text = "삭제",
+                    style = StaticTypeScale.Default.body4,
+                    color = Color(0xFFF7F7F8),
+                )
+
+                HorizontalDivider(
+                    modifier = Modifier.fillMaxWidth(),
+                )
+
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    text = "신고",
+                    style = StaticTypeScale.Default.body4,
+                    color = Color(0xFFF7F7F8),
+                )
+
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    text = "차단",
+                    style = StaticTypeScale.Default.body4,
+                    color = Color(0xFFF7F7F8),
+                )
+            }
+        }
+    }
+
     if (showMessageDialog) {
         Dialog(onDismissRequest = { }) {
             Box(
-                modifier = Modifier.size(width = 310.dp, height = 376.dp),
+                modifier = Modifier
+                    .width(296.dp)
+                    .heightIn(min = 376.dp, max = 424.dp),
             ) {
                 Column(
                     modifier = Modifier
@@ -157,11 +208,11 @@ fun MessageStorageScreen(
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(top = 8.dp, end = 8.dp)
-                        .clickable { showMessageDialog = false },
+                        .padding(top = 8.dp, end = 8.dp),
                     contentAlignment = Alignment.CenterEnd,
                 ) {
                     Image(
+                        modifier = Modifier.clickable { showMessageDialog = false },
                         painter = painterResource(id = R.drawable.close),
                         contentDescription = stringResource(id = R.string.close),
                     )
