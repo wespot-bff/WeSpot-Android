fc4d56a91e58dae3a6f738e2ec533656ce9293b7
jeongjaino <akgk155@gmail.com>
2024-07-28 16:10:44 +0900

    [FEATURE]#43 : Message DTO 변경

--- core/model/src/main/kotlin/com/bff/wespot/model/message/response/Message.kt
+++ core/model/src/main/kotlin/com/bff/wespot/model/message/response/Message.kt
@@ -5,9 +5,16 @@ import java.time.LocalDateTime
 data class Message(
     val id: Int,
     val senderName: String,
+    val receiverId: Int,
+    val receiverName: String,
+    val receiverSchoolName: String,
+    val receiverGrade: Int,
+    val receiverClassNumber: Int,
     val content: String,
-    val receivedAt: LocalDateTime,
+    val receivedAt: LocalDateTime?,
     val isRead: Boolean,
-    val isBlocked: Boolean,
+    val isBlocked: Boolean = false,
     val readAt: LocalDateTime?,
-)
+) {
+    constructor() : this(-1, "", -1, "", "", -1, -1, "", LocalDateTime.MAX, false, false, null)
+}
