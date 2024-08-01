fa470730c44184ecb2ff6e3d32f40c52cb10ae72
jeongjaino <akgk155@gmail.com>
2024-08-02 00:30:49 +0900

    [FEATURE]#65 : 전체 탭 공통 리스트 아이템 컴포넌트 구현

---
+++ feature/entire/src/main/java/com/bff/wespot/entire/screen/component/EntireListItem.kt
@@ -0,0 +1,45 @@
+package com.bff.wespot.entire.screen.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.bff.wespot.designsystem.R
+import com.bff.wespot.designsystem.theme.StaticTypeScale
+import com.bff.wespot.designsystem.theme.WeSpotThemeManager
+
+@Composable
+internal fun EntireListItem(
+    text: String,
+    onClick: () -> Unit,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onClick() },
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 10.dp),
+            text = text,
+            style = StaticTypeScale.Default.body4,
+            color = WeSpotThemeManager.colors.txtTitleColor,
+        )
+
+        Icon(
+            painter = painterResource(id = R.drawable.right_arrow),
+            contentDescription = stringResource(id = R.string.right_arrow),
+            tint = WeSpotThemeManager.colors.abledIconColor,
+        )
+    }
+}
