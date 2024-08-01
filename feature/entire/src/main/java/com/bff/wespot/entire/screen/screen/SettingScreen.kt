f5e0941945032997cfb3e0a28dfd589ace83c394
jeongjaino <akgk155@gmail.com>
2024-08-02 00:31:16 +0900

    [FEATURE]#65 : 전체 설정 화면 구현

---
+++ feature/entire/src/main/java/com/bff/wespot/entire/screen/screen/SettingScreen.kt
@@ -0,0 +1,93 @@
+package com.bff.wespot.entire.screen.screen
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import com.bff.wespot.designsystem.component.header.WSTopBar
+import com.bff.wespot.designsystem.theme.WeSpotThemeManager
+import com.bff.wespot.entire.R
+import com.bff.wespot.entire.screen.component.EntireListItem
+import com.ramcosta.composedestinations.annotation.Destination
+
+interface SettingNavigator {
+    fun navigateUp()
+    fun navigateToNotificationSetting()
+    fun navigateToAccountSetting()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Destination
+@Composable
+fun SettingScreen(
+    navigator: SettingNavigator,
+) {
+    Scaffold(
+        topBar = {
+            WSTopBar(
+                title = "",
+                canNavigateBack = true,
+                navigateUp = { navigator.navigateUp() },
+                action = {
+                    Icon(
+                        modifier = Modifier.clickable { navigator.navigateToNotificationSetting() },
+                        imageVector = ImageVector.vectorResource(R.drawable.setting),
+                        contentDescription = stringResource(R.string.setting_icon),
+                        tint = WeSpotThemeManager.colors.secondaryBtnColor,
+                    )
+                },
+            )
+        },
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+                .padding(top = 8.dp, start = 24.dp, end = 24.dp),
+            verticalArrangement = Arrangement.spacedBy(24.dp),
+        ) {
+            EntireListItem(text = stringResource(R.string.notification_setting)) {
+            }
+
+            HorizontalDivider(
+                modifier = Modifier.fillMaxWidth(),
+                thickness = 1.dp,
+                color = WeSpotThemeManager.colors.cardBackgroundColor,
+            )
+
+            EntireListItem(text = stringResource(R.string.privacy_policy)) {
+            }
+
+            EntireListItem(text = stringResource(R.string.terms_of_service)) {
+            }
+
+            EntireListItem(text = stringResource(R.string.latest_updates)) {
+            }
+
+            HorizontalDivider(
+                modifier = Modifier.fillMaxWidth(),
+                thickness = 1.dp,
+                color = WeSpotThemeManager.colors.cardBackgroundColor,
+            )
+
+            EntireListItem(text = stringResource(R.string.block_list)) {
+            }
+
+            EntireListItem(text = stringResource(R.string.account_setting)) {
+                navigator.navigateToAccountSetting()
+            }
+        }
+    }
+}
