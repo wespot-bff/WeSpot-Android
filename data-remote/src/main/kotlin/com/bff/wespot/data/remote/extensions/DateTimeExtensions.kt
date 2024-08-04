43e3101af58addaef8f39fed29cdd28f4de46e74
jeongjaino <akgk155@gmail.com>
2024-08-04 15:36:23 +0900

    [FEATURE]#75 : 파싱 예외 케이스 하나 추가 ISO 타입이 아닌 경우,

--- data-remote/src/main/kotlin/com/bff/wespot/data/remote/extensions/DateTimeExtensions.kt
+++ data-remote/src/main/kotlin/com/bff/wespot/data/remote/extensions/DateTimeExtensions.kt
@@ -6,4 +6,7 @@ import java.time.format.DateTimeFormatter
 fun String.toISOLocalDateTime(): LocalDateTime? =
     runCatching {
         LocalDateTime.parse(this, DateTimeFormatter.ISO_DATE_TIME)
+    }.recoverCatching {
+        val correctedString = this.replace(" ", "T")
+        LocalDateTime.parse(correctedString, DateTimeFormatter.ISO_DATE_TIME)
     }.getOrNull()
