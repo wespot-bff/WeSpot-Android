9ef70c64f3dd4bb5720d6f5ff5c48de601ed6dc3
flash159483 <seungwonjung0313@gmail.com>
2024-08-04 17:59:46 +0900

    [FEAT]#69: Fold로 교체

--- domain/src/main/kotlin/com/bff/wespot/domain/usecase/CheckProfanityUseCase.kt
+++ domain/src/main/kotlin/com/bff/wespot/domain/usecase/CheckProfanityUseCase.kt
@@ -8,18 +8,16 @@ class CheckProfanityUseCase @Inject constructor(
     private val commonRepository: CommonRepository,
 ) {
     suspend operator fun invoke(content: String): Boolean {
-        val result = commonRepository.checkProfanity(content)
-
-        return if (result.isSuccess) {
-            false
-        } else {
-            val exception = result.exceptionOrNull()
-            val networkException = exception as? NetworkException
-            if (networkException != null && networkException.status == 400) {
-                true
-            } else {
-                throw exception!!
-            }
-        }
+        return commonRepository.checkProfanity(content).fold(
+            onSuccess = { false },
+            onFailure = { exception ->
+                val networkException = exception as? NetworkException
+                if (networkException != null && networkException.status == 400) {
+                    true
+                } else {
+                    throw exception
+                }
+            },
+        )
     }
 }
