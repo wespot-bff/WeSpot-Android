95bf01c82780dd2e454958a98345e0d126cd2162
jeongjaino <akgk155@gmail.com>
2024-08-07 19:10:24 +0900

    [FEATURE]#61 : 프로필 캐릭터 수정 화면 구현

---
+++ feature/entire/src/main/java/com/bff/wespot/entire/screen/screen/edit/CharacterEditScreen.kt
@@ -0,0 +1,96 @@
+package com.bff.wespot.entire.screen.screen.edit
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.bff.wespot.designsystem.component.header.WSTopBar
+import com.bff.wespot.entire.screen.state.EntireAction
+import com.bff.wespot.entire.screen.state.EntireSideEffect
+import com.bff.wespot.entire.screen.viewmodel.EntireViewModel
+import com.bff.wespot.model.user.response.ProfileCharacter
+import com.bff.wespot.ui.CharacterScreen
+import com.ramcosta.composedestinations.annotation.Destination
+import org.orbitmvi.orbit.compose.collectAsState
+import org.orbitmvi.orbit.compose.collectSideEffect
+
+interface CharacterEditNavigator {
+    fun navigateUp()
+    fun navigateToProfileEditScreen(args: ProfileEditNavArgs)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Destination
+@Composable
+fun CharacterEditScreen(
+    navigator: CharacterEditNavigator,
+    viewModel: EntireViewModel = hiltViewModel(),
+) {
+    val color by viewModel.backgroundColor.collectAsStateWithLifecycle()
+    val character by viewModel.characters.collectAsStateWithLifecycle()
+
+    val action = viewModel::onAction
+    val state by viewModel.collectAsState()
+    viewModel.collectSideEffect {
+        when (it) {
+            EntireSideEffect.NavigateToEntire -> {
+                navigator.navigateToProfileEditScreen(
+                    ProfileEditNavArgs(true),
+                )
+            }
+            else -> { }
+        }
+    }
+
+    if (color.isEmpty() || character.isEmpty()) {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            CircularProgressIndicator()
+        }
+        return
+    }
+
+    Scaffold(
+        topBar = {
+            WSTopBar(
+                title = "",
+                canNavigateBack = true,
+                navigateUp = { navigator.navigateUp() },
+            )
+        },
+    ) {
+        Box(modifier = Modifier.padding(it)) {
+            CharacterScreen(
+                name = state.profile.name,
+                isEditing = true,
+                characterList = character,
+                colorList = color,
+            ) { iconUrl, color ->
+                action(
+                    EntireAction.OnCharacterEditDoneButtonClicked(
+                        ProfileCharacter(iconUrl, color),
+                    ),
+                )
+            }
+        }
+    }
+
+    if (state.isLoading) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .clickable(enabled = false) { },
+            contentAlignment = Alignment.Center,
+        ) {
+            CircularProgressIndicator()
+        }
+    }
+}
