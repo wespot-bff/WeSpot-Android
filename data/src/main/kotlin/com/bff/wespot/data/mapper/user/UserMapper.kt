20f2bc21658e02ad47bf5c6e265a6d571b6d6d84
jeongjaino <akgk155@gmail.com>
2024-08-07 23:29:11 +0900

    [FEATURE]#63 : 알림 설정 DTO Mapper 구현

---
+++ data/src/main/kotlin/com/bff/wespot/data/mapper/user/UserMapper.kt
@@ -0,0 +1,10 @@
+package com.bff.wespot.data.mapper.user
+
+import com.bff.wespot.data.remote.model.user.response.NotificationSettingDto
+import com.bff.wespot.model.user.response.NotificationSetting
+
+internal fun NotificationSetting.toNotificationSettingDto() = NotificationSettingDto(
+    isEnableVoteNotification = isEnableVoteNotification,
+    isEnableMessageNotification = isEnableMessageNotification,
+    isEnableEventNotification = isEnableEventNotification,
+)
