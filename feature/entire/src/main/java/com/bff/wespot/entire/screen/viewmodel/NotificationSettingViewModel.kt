38dc1ac608208b7f0c6fe90ccf4a6c3083881f7a
jeongjaino <akgk155@gmail.com>
2024-08-07 23:32:21 +0900

    [FEATURE]#63 : 설정된 알림 조회 및 알림 설정 수정 로직 구현

---
+++ feature/entire/src/main/java/com/bff/wespot/entire/screen/viewmodel/NotificationSettingViewModel.kt
@@ -0,0 +1,88 @@
+package com.bff.wespot.entire.screen.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bff.wespot.domain.repository.user.UserRepository
+import com.bff.wespot.entire.screen.state.notification.NotificationSettingAction
+import com.bff.wespot.entire.screen.state.notification.NotificationSettingUiState
+import com.bff.wespot.model.user.response.NotificationSetting
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import org.orbitmvi.orbit.ContainerHost
+import org.orbitmvi.orbit.syntax.simple.intent
+import org.orbitmvi.orbit.syntax.simple.reduce
+import org.orbitmvi.orbit.viewmodel.container
+import timber.log.Timber
+import javax.inject.Inject
+
+@HiltViewModel
+class NotificationSettingViewModel @Inject constructor(
+    private val userRepository: UserRepository,
+) : ViewModel(), ContainerHost<NotificationSettingUiState, NotificationSettingAction> {
+    override val container = container<NotificationSettingUiState, NotificationSettingAction>(
+        NotificationSettingUiState(),
+    )
+
+    fun onAction(action: NotificationSettingAction) {
+        when (action) {
+            NotificationSettingAction.OnNotificationSettingScreenEntered ->
+                getNotificationSetting()
+            is NotificationSettingAction.OnVoteNotificationSwitched -> {
+                handleVoteNotificationSwitched(action.isSwitched)
+            }
+            is NotificationSettingAction.OnMessageNotificationSwitched -> {
+                handleMessageNotificationSwitched(action.isSwitched)
+            }
+            is NotificationSettingAction.OnEventNotificationSwitched -> {
+                handleEventNotificationSwitched(action.isSwitched)
+            }
+            NotificationSettingAction.OnNotificationSettingScreenExited -> {
+                postNotificationSetting()
+            }
+        }
+    }
+
+    private fun getNotificationSetting() = intent {
+        viewModelScope.launch {
+            userRepository.getNotificationSetting()
+                .onSuccess { setting ->
+                    reduce {
+                        state.copy(
+                            isEnableVoteNotification = setting.isEnableVoteNotification,
+                            isEnableMessageNotification = setting.isEnableMessageNotification,
+                            isEnableEventNotification = setting.isEnableEventNotification,
+                        )
+                    }
+                }
+                .onFailure {
+                    Timber.e(it)
+                }
+        }
+    }
+
+    private fun handleVoteNotificationSwitched(isSwitched: Boolean) = intent {
+        reduce { state.copy(isEnableVoteNotification = isSwitched) }
+    }
+
+    private fun handleMessageNotificationSwitched(isSwitched: Boolean) = intent {
+        reduce { state.copy(isEnableMessageNotification = isSwitched) }
+    }
+
+    private fun handleEventNotificationSwitched(isSwitched: Boolean) = intent {
+        reduce { state.copy(isEnableEventNotification = isSwitched) }
+    }
+
+    private fun postNotificationSetting() = intent {
+        viewModelScope.launch {
+            userRepository.updateNotificationSetting(
+                NotificationSetting(
+                    isEnableVoteNotification = state.isEnableVoteNotification,
+                    isEnableMessageNotification = state.isEnableMessageNotification,
+                    isEnableEventNotification = state.isEnableEventNotification,
+                ),
+            ).onFailure {
+                Timber.e(it)
+            }
+        }
+    }
+}
