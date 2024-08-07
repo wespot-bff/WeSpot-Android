0204aaf22cfd230313ff7f04a4e9ab930f503db6
jeongjaino <akgk155@gmail.com>
2024-08-07 19:10:11 +0900

    [FEATURE]#61 : 프로필 캐릭터 수정 Intent 구현

--- feature/entire/src/main/java/com/bff/wespot/entire/screen/state/EntireAction.kt
+++ feature/entire/src/main/java/com/bff/wespot/entire/screen/state/EntireAction.kt
@@ -1,17 +1,21 @@
 package com.bff.wespot.entire.screen.state
 
+import com.bff.wespot.model.user.response.ProfileCharacter
+
 sealed class EntireAction {
     data object OnEntireScreenEntered : EntireAction()
     data object OnRevokeScreenEntered : EntireAction()
+    data object OnCharacterEditScreenEntered : EntireAction()
     data object OnProfileEditScreenEntered : EntireAction()
     data object OnBlockListScreenEntered : EntireAction()
     data object OnRevokeConfirmed : EntireAction()
     data object OnSignOutButtonClicked : EntireAction()
     data object OnRevokeButtonClicked : EntireAction()
     data object OnIntroductionEditDoneButtonClicked : EntireAction()
+    data object UnBlockMessage : EntireAction()
     data class OnProfileEditTextFieldFocused(val focused: Boolean) : EntireAction()
     data class OnIntroductionChanged(val introduction: String) : EntireAction()
-    data object UnBlockMessage : EntireAction()
     data class OnUnBlockButtonClicked(val messageId: Int) : EntireAction()
     data class OnRevokeReasonSelected(val reason: String) : EntireAction()
+    data class OnCharacterEditDoneButtonClicked(val character: ProfileCharacter) : EntireAction()
 }
--- feature/entire/src/main/java/com/bff/wespot/entire/screen/state/EntireSideEffect.kt
+++ feature/entire/src/main/java/com/bff/wespot/entire/screen/state/EntireSideEffect.kt
@@ -2,5 +2,6 @@ package com.bff.wespot.entire.screen.state
 
 sealed class EntireSideEffect {
     data object NavigateToAuth : EntireSideEffect()
-    data class ShowToast(val message: String) : EntireSideEffect()
+    data object NavigateToEntire : EntireSideEffect()
+    data object ShowToast : EntireSideEffect()
 }
