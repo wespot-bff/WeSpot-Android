4c426d071bb3206995dc8eb93f97190da5337963
jeongjaino <akgk155@gmail.com>
2024-08-08 00:09:26 +0900

    [CHORE]#75 : 주석 제거

--- app/src/main/kotlin/com/bff/wespot/PushNotificationService.kt
+++ app/src/main/kotlin/com/bff/wespot/PushNotificationService.kt
@@ -6,13 +6,9 @@ import com.google.firebase.messaging.RemoteMessage
 class PushNotificationService : FirebaseMessagingService() {
     override fun onNewToken(token: String) {
         super.onNewToken(token)
-
-        // Update server
     }
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
-
-        // Respond to received messages
     }
 }
