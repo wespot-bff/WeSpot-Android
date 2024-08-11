9929112df74b6b4699f3a809970e74f12147b647
jeongjaino <akgk155@gmail.com>
2024-08-11 16:02:04 +0900

    [FEATURE]#76 : Main으로 Naviaget 시 파라미터 추가

--- core/navigation/src/main/kotlin/com/bff/wespot/navigation/Navigator.kt
+++ core/navigation/src/main/kotlin/com/bff/wespot/navigation/Navigator.kt
@@ -5,8 +5,12 @@ import android.content.Intent
 import android.net.Uri
 
 interface Navigator {
-
-    fun navigateToMain(context: Context) : Intent
+    fun navigateToMain(
+        context: Context,
+        targetId: Pair<String, Int>,
+        date: Pair<String, String>,
+        type: Pair<String, String>,
+    ) : Intent
 
     fun navigateToAuth(context: Context): Intent
 
