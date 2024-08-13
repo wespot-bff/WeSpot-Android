92b4d52b380d6c71e4f9c88f462ef52c8f1768fe
jeongjaino <akgk155@gmail.com>
2024-08-14 00:01:11 +0900

    [FEATURE]#60 : 전체 탭 각 페이지 웹링크 전환 구현

--- feature/entire/src/main/java/com/bff/wespot/entire/screen/setting/SettingScreen.kt
+++ feature/entire/src/main/java/com/bff/wespot/entire/screen/setting/SettingScreen.kt
@@ -1,6 +1,5 @@
 package com.bff.wespot.entire.screen.setting
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,12 +9,15 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.bff.wespot.designsystem.component.header.WSTopBar
 import com.bff.wespot.designsystem.theme.WeSpotThemeManager
 import com.bff.wespot.entire.R
 import com.bff.wespot.entire.component.EntireListItem
+import com.bff.wespot.navigation.Navigator
+import com.bff.wespot.navigation.util.WebLink
 import com.ramcosta.composedestinations.annotation.Destination
 
 interface SettingNavigator {
@@ -30,7 +32,10 @@ interface SettingNavigator {
 @Composable
 fun SettingScreen(
     navigator: SettingNavigator,
+    activityNavigator: Navigator,
 ) {
+    val context = LocalContext.current
+
     Scaffold(
         topBar = {
             WSTopBar(
@@ -45,29 +50,31 @@ fun SettingScreen(
                 .fillMaxSize()
                 .padding(it)
                 .padding(top = 8.dp, start = 24.dp, end = 24.dp),
-            verticalArrangement = Arrangement.spacedBy(24.dp),
         ) {
             EntireListItem(text = stringResource(R.string.notification_setting)) {
                 navigator.navigateToNotificationSetting()
             }
 
             HorizontalDivider(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth().padding(vertical = 12.dp),
                 thickness = 1.dp,
                 color = WeSpotThemeManager.colors.cardBackgroundColor,
             )
 
             EntireListItem(text = stringResource(R.string.privacy_policy)) {
+                activityNavigator.navigateToWebLink(context, WebLink.PRIVACY_POLICY)
             }
 
             EntireListItem(text = stringResource(R.string.terms_of_service)) {
+                activityNavigator.navigateToWebLink(context, WebLink.TERMS_OF_SERVICE)
             }
 
             EntireListItem(text = stringResource(R.string.latest_updates)) {
+                activityNavigator.navigateToWebLink(context, WebLink.PLAY_STORE)
             }
 
             HorizontalDivider(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth().padding(vertical = 12.dp),
                 thickness = 1.dp,
                 color = WeSpotThemeManager.colors.cardBackgroundColor,
             )
