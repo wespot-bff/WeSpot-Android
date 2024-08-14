8cf8554fe73e5356ac664cae36554645ca604b8e
jeongjaino <akgk155@gmail.com>
2024-08-15 01:32:28 +0900

    [FEATURE]#97: sender -> senderName Dto 변경

--- core/model/src/main/kotlin/com/bff/wespot/model/message/request/SentMessage.kt
+++ core/model/src/main/kotlin/com/bff/wespot/model/message/request/SentMessage.kt
@@ -3,6 +3,6 @@ package com.bff.wespot.model.message.request
 data class SentMessage(
     val receiverId: Int,
     val content: String,
-    val sender: String,
+    val senderName: String,
     val isAnonymous: Boolean,
 )
