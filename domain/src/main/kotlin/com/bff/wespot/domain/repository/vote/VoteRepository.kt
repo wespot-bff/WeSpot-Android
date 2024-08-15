0cc7e0cbca3dd33cf66932a252d10940927bd050
flash159483 <seungwonjung0313@gmail.com>
2024-08-15 14:47:39 +0900

    [FEAT]#86: Base 바탕으로 repository를 새로 추가해요

--- domain/src/main/kotlin/com/bff/wespot/domain/repository/vote/VoteRepository.kt
+++ domain/src/main/kotlin/com/bff/wespot/domain/repository/vote/VoteRepository.kt
@@ -4,17 +4,13 @@ import com.bff.wespot.model.vote.request.VoteResultsUpload
 import com.bff.wespot.model.vote.response.IndividualReceived
 import com.bff.wespot.model.vote.response.IndividualSent
 import com.bff.wespot.model.vote.response.VoteItems
-import com.bff.wespot.model.vote.response.VoteReceived
 import com.bff.wespot.model.vote.response.VoteResults
-import com.bff.wespot.model.vote.response.VoteSent
 
 interface VoteRepository {
     suspend fun getVoteQuestions(): Result<VoteItems>
     suspend fun uploadVoteResults(voteResults: VoteResultsUpload): Boolean
     suspend fun getVoteResults(date: String): Result<VoteResults>
     suspend fun getFirstVoteResults(date: String): Result<VoteResults>
-    suspend fun getVoteSent(): Result<VoteSent>
-    suspend fun getVoteReceived(): Result<VoteReceived>
     suspend fun getReceivedVote(
         date: String,
         optionId: Int,
