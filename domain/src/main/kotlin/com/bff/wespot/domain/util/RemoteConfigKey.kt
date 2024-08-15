d79620f122373a314cb3cbde78538eb013c31d76
flash159483 <seungwonjung0313@gmail.com>
2024-08-15 20:18:27 +0900

    [FEAT]#96: 서버 주소 RemoteConfig으로 가져오기

--- domain/src/main/kotlin/com/bff/wespot/domain/util/RemoteConfigKey.kt
+++ domain/src/main/kotlin/com/bff/wespot/domain/util/RemoteConfigKey.kt
@@ -2,4 +2,6 @@ package com.bff.wespot.domain.util
 
 object RemoteConfigKey {
     const val MIN_VERSION = "MIN_VERSION"
+    const val BASE_URL = "BASE_URL"
+    const val MOCK_BASE_URL = "MOCK_BASE_URL"
 }
