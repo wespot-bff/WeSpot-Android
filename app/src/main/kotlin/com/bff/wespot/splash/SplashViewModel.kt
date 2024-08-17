fe55923885d688380d7a6db68bad10fc74ad80d0
flash159483 <seungwonjung0313@gmail.com>
2024-08-17 18:06:55 +0900

    [CHORE]#110: Splash 버전 대응

--- app/src/main/kotlin/com/bff/wespot/splash/SplashViewModel.kt
+++ app/src/main/kotlin/com/bff/wespot/splash/SplashViewModel.kt
@@ -3,6 +3,7 @@ package com.bff.wespot.splash
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.bff.wespot.domain.repository.RemoteConfigRepository
+import com.bff.wespot.domain.util.RemoteConfigKey
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -16,9 +17,15 @@ class SplashViewModel @Inject constructor(
     private val _start = MutableStateFlow(false)
     val start = _start.asStateFlow()
 
+    private val _minVersion = MutableStateFlow("")
+    val minVersion = _minVersion.asStateFlow()
+
     init {
         viewModelScope.launch {
             _start.value = dataSource.startRemoteConfig()
+            if (_start.value) {
+                _minVersion.emit(dataSource.fetchFromRemoteConfig(RemoteConfigKey.MIN_VERSION))
+            }
         }
     }
 }
