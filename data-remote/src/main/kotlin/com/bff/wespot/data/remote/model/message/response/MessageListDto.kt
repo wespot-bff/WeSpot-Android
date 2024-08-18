92f89ac5750002f53b49d1063948213b4c2a226a
jeongjaino <akgk155@gmail.com>
2024-08-18 19:32:31 +0900

    [FEATURE]#110 : 쪽지 리스트 DTO LastCursorId 추가

--- data-remote/src/main/kotlin/com/bff/wespot/data/remote/model/message/response/MessageListDto.kt
+++ data-remote/src/main/kotlin/com/bff/wespot/data/remote/model/message/response/MessageListDto.kt
@@ -6,10 +6,12 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class MessageListDto (
     val messages: List<MessageDto>,
+    val lastCursorId: Int,
     val hasNext: Boolean,
 ) {
     fun toMessageList(): MessageList = MessageList(
         messages = messages.map { it.toMessage() },
+        lastCursorId = lastCursorId,
         hasNext = hasNext,
     )
 }
