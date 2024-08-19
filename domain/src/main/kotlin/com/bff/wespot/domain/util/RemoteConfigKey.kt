9efcb45f265c20faaababaa9da69db4804fa72fb
jeongjaino <akgk155@gmail.com>
2024-08-20 02:15:25 +0900

    [CHORE]#110 : RemoteConfig 추가

--- domain/src/main/kotlin/com/bff/wespot/domain/util/RemoteConfigKey.kt
+++ domain/src/main/kotlin/com/bff/wespot/domain/util/RemoteConfigKey.kt
@@ -4,4 +4,5 @@ object RemoteConfigKey {
     const val MIN_VERSION = "MIN_VERSION"
     const val BASE_URL = "BASE_URL"
     const val MOCK_BASE_URL = "MOCK_BASE_URL"
+    const val VOTE_QUESTION_GOOGLE_FORM_URL = "VOTE_QUESTION_GOOGLE_FORM_URL"
 }
