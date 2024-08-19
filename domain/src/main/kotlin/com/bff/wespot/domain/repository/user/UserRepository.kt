d3e5444bd225c8c0e27a133255c465faee3b9c5c
jeongjaino <akgk155@gmail.com>
2024-08-19 23:24:44 +0900

    [FEATURE]#110 : 기존 유저 조회 레포 메소드 삭제

--- domain/src/main/kotlin/com/bff/wespot/domain/repository/user/UserRepository.kt
+++ domain/src/main/kotlin/com/bff/wespot/domain/repository/user/UserRepository.kt
@@ -3,11 +3,8 @@ package com.bff.wespot.domain.repository.user
 import com.bff.wespot.model.user.response.NotificationSetting
 import com.bff.wespot.model.user.response.Profile
 import com.bff.wespot.model.user.response.ProfileCharacter
-import com.bff.wespot.model.user.response.User
 
 interface UserRepository {
-    suspend fun getUserListByName(name: String, cursorId: Int): Result<List<User>>
-
     suspend fun getProfile(): Result<Profile>
 
     suspend fun getNotificationSetting(): Result<NotificationSetting>
