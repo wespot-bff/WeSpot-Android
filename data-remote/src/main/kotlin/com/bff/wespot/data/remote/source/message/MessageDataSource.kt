059e5c4ddd23d6a8b20340d6bd4eff29b503157a
jeongjaino <akgk155@gmail.com>
2024-08-19 22:31:47 +0900

    [FEATURE]#110 : 쪽지 조회 API 분리

--- data-remote/src/main/kotlin/com/bff/wespot/data/remote/source/message/MessageDataSource.kt
+++ data-remote/src/main/kotlin/com/bff/wespot/data/remote/source/message/MessageDataSource.kt
@@ -1,25 +1,22 @@
 package com.bff.wespot.data.remote.source.message
 
-import com.bff.wespot.data.remote.model.message.request.MessageTypeDto
-import com.bff.wespot.data.remote.model.message.request.SentMessageDto
+import com.bff.wespot.data.remote.model.message.request.WrittenMessageDto
 import com.bff.wespot.data.remote.model.message.response.BlockedMessageListDto
 import com.bff.wespot.data.remote.model.message.response.MessageDto
 import com.bff.wespot.data.remote.model.message.response.MessageIdDto
 import com.bff.wespot.data.remote.model.message.response.MessageListDto
 import com.bff.wespot.data.remote.model.message.response.MessageStatusDto
-import com.bff.wespot.model.message.response.Message
 
 interface MessageDataSource {
-    suspend fun getMessageList(
-        messageTypeDto: MessageTypeDto,
-        cursorId: Int,
-    ): Result<MessageListDto>
+    suspend fun getReceivedMessageList(cursorId: Int?): Result<MessageListDto>
 
-    suspend fun postMessage(sentMessageDto: SentMessageDto): Result<MessageIdDto>
+    suspend fun getSentMessageList(cursorId: Int?): Result<MessageListDto>
+
+    suspend fun postMessage(sentMessageDto: WrittenMessageDto): Result<MessageIdDto>
 
     suspend fun getMessageStatus(): Result<MessageStatusDto>
 
-    suspend fun editMessage(messageId: Int, sentMessageDto: SentMessageDto): Result<Unit>
+    suspend fun editMessage(messageId: Int, sentMessageDto: WrittenMessageDto): Result<Unit>
 
     suspend fun getMessage(messageId: Int): Result<MessageDto>
 
