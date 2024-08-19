e535345222800919c1ce1e8a11a9a88c8c570b0a
jeongjaino <akgk155@gmail.com>
2024-08-20 00:56:36 +0900

    [FEATURE]#110 : 알림 리스트 화면 페이징 적용

--- feature/notification/src/main/kotlin/com/bff/wespot/notification/state/NotificationUiState.kt
+++ feature/notification/src/main/kotlin/com/bff/wespot/notification/state/NotificationUiState.kt
@@ -1,7 +1,10 @@
 package com.bff.wespot.notification.state
 
+import androidx.paging.PagingData
 import com.bff.wespot.model.notification.Notification
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 data class NotificationUiState(
-    val notificationList: List<Notification> = listOf(),
+    val notificationList: Flow<PagingData<Notification>> = flow { },
 )
