22b94571459c3f906b5adaf7b85cade37ecf6e60
jeongjaino <akgk155@gmail.com>
2024-08-22 22:47:56 +0900

    [FIX] : 알림 리스트 커서 아이디 설정

--- data-remote/src/main/kotlin/com/bff/wespot/data/remote/source/notification/NotificationDataSource.kt
+++ data-remote/src/main/kotlin/com/bff/wespot/data/remote/source/notification/NotificationDataSource.kt
@@ -3,7 +3,7 @@ package com.bff.wespot.data.remote.source.notification
 import com.bff.wespot.data.remote.model.notification.NotificationListDto
 
 interface NotificationDataSource {
-    suspend fun getNotification(): Result<NotificationListDto>
+    suspend fun getNotification(cursorId: Int?): Result<NotificationListDto>
 
     suspend fun updateNotificationReadStatus(id: Int): Result<Unit>
 }
--- data/src/main/kotlin/com/bff/wespot/data/repository/notification/NotificationRepositoryImpl.kt
+++ data/src/main/kotlin/com/bff/wespot/data/repository/notification/NotificationRepositoryImpl.kt
@@ -8,13 +8,6 @@ import javax.inject.Inject
 class NotificationRepositoryImpl @Inject constructor(
     private val notificationDataSource: NotificationDataSource,
 ): NotificationRepository {
-    override suspend fun getNotificationList(): Result<List<Notification>> =
-        notificationDataSource.getNotification().mapCatching {
-            it.notifications.map { notificationDto ->
-                notificationDto.toNotification()
-            }
-        }
-
     override suspend fun updateNotificationReadStatus(id: Int): Result<Unit> =
         notificationDataSource.updateNotificationReadStatus(id)
 }
