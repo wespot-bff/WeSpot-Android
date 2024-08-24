72228b7ee8771a8575ebf3bba252da5a3ff243d7
jeongjaino <akgk155@gmail.com>
2024-08-25 00:50:31 +0900

    [FEATURE]#128 : 클릭 설정 구현

--- feature/entire/src/main/java/com/bff/wespot/entire/screen/EntireScreen.kt
+++ feature/entire/src/main/java/com/bff/wespot/entire/screen/EntireScreen.kt
@@ -2,6 +2,7 @@ package com.bff.wespot.entire.screen
 
 import android.graphics.Color.parseColor
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
