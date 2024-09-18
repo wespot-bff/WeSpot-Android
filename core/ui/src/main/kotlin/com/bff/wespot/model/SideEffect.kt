517652417f86ae6268b13f0499a890373b21b643
jeongjaino <akgk155@gmail.com>
2024-09-18 21:06:14 +0900

    [FEATURE]#155 : BaseSideEffect to SideEffect renaming

---
+++ core/ui/src/main/kotlin/com/bff/wespot/model/SideEffect.kt
@@ -0,0 +1,19 @@
+package com.bff.wespot.model
+
+import com.bff.wespot.model.exception.NetworkException
+import com.bff.wespot.model.exception.NetworkExceptionViewType
+
+sealed class SideEffect {
+    data object Redirect : SideEffect()
+    data class ShowToast(val message: String) : SideEffect()
+    data class ShowDialog(val message: String) : SideEffect()
+
+    companion object {
+        fun NetworkException.toSideEffect(): SideEffect =
+            when (this.viewType) {
+                NetworkExceptionViewType.TOAST -> ShowToast(this.detail)
+                NetworkExceptionViewType.DIALOG -> ShowDialog(this.detail)
+                NetworkExceptionViewType.REDIRECT -> Redirect
+            }
+    }
+}
