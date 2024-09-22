ffeb0da17397cabdce4192c3759c1192b8459d7a
jeongjaino <akgk155@gmail.com>
2024-09-22 19:05:39 +0900

    [REFACTOR]#160 : Start Stop Lifecycle에 따라 Timer 상태 변경

--- feature/message/src/main/kotlin/com/bff/wespot/message/state/MessageAction.kt
+++ feature/message/src/main/kotlin/com/bff/wespot/message/state/MessageAction.kt
@@ -1,6 +1,7 @@
 package com.bff.wespot.message.state
 
 sealed class MessageAction {
-    data object OnHomeScreenEntered : MessageAction()
+    data object StartTimeTracking : MessageAction()
+    data object CancelTimeTracking : MessageAction()
     data object OnReservedMessageScreenEntered : MessageAction()
 }
