3fa3021201e4e611fa457ed70823164064cfe7e2
jeongjaino <akgk155@gmail.com>
2024-09-24 22:56:18 +0900

    [REFACTOR]#160 : Message, ReceivedMessage, BaseMessage 상속받도록 수정

--- core/model/src/main/kotlin/com/bff/wespot/model/message/response/Message.kt
+++ core/model/src/main/kotlin/com/bff/wespot/model/message/response/Message.kt
@@ -7,18 +7,18 @@ import java.time.LocalDateTime
  * 보낸 쪽지, 예약된 쪽지, 쪽지 조회에서 사용되는 전체 필드를 가진 Model
  */
 data class Message(
-    val id: Int,
-    val senderName: String,
-    val receiver: User,
-    val content: String,
-    val receivedAt: LocalDateTime?,
-    val isRead: Boolean,
+    override val id: Int,
+    override val senderName: String,
+    override val receiver: User,
+    override val content: String,
+    override val receivedAt: LocalDateTime?,
+    override val isRead: Boolean,
+    override val readAt: LocalDateTime?,
     val isReported: Boolean,
     val isBlocked: Boolean,
     val isAnonymous: Boolean,
-    val readAt: LocalDateTime?,
-) {
-    constructor() : this(-1, "", User(), "", LocalDateTime.MAX, false, false, false, false, null)
+) : BaseMessage(id, senderName, receiver, content, receivedAt, isRead, readAt) {
+    constructor() : this(-1, "", User(), "", LocalDateTime.MAX, false, null, false, false, false)
 
     fun toReceivedMessage(): ReceivedMessage = ReceivedMessage(
         id = id,
