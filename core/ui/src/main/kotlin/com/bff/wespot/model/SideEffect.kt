1fdc5d31988b4a30de7aeef2b2df2a4127095aea
jeongjaino <akgk155@gmail.com>
2024-09-29 23:36:54 +0900

    [FEATURE]#151 : SideEffect Default Toast 추가

--- core/ui/src/main/kotlin/com/bff/wespot/model/SideEffect.kt
+++ core/ui/src/main/kotlin/com/bff/wespot/model/SideEffect.kt
@@ -10,11 +10,15 @@ sealed class SideEffect {
     data class ShowDialog(val message: String) : SideEffect()
 
     companion object {
+        private const val DEFAULT_TOAST_MESSAGE = "알 수 없는 에러가 발생하였습니다."
+
         fun NetworkException.toSideEffect(): SideEffect =
             when (this.viewType) {
                 NetworkExceptionViewType.TOAST -> ShowToast(this.detail)
                 NetworkExceptionViewType.DIALOG -> ShowDialog(this.detail)
                 NetworkExceptionViewType.REDIRECT -> Redirect
             }
+
+        fun toToastEffect(message: String = DEFAULT_TOAST_MESSAGE) = ShowToast(message)
     }
 }
