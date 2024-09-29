8f49ae2bd12f6b4bc43cf55d86e46a63e63f1306
jeongjaino <akgk155@gmail.com>
2024-09-29 22:36:52 +0900

    [FEATURE]#151 : 읽지 않은 쪽지만 조회하도록 수정

--- feature/message/src/main/kotlin/com/bff/wespot/message/viewmodel/StorageViewModel.kt
+++ feature/message/src/main/kotlin/com/bff/wespot/message/viewmodel/StorageViewModel.kt
@@ -188,7 +188,7 @@ class StorageViewModel @Inject constructor(
             state.copy(clickedMessage = message)
         }
 
-        if (type == MessageType.RECEIVED) {
+        if (type == MessageType.RECEIVED || message.isRead.not()) {
             updateMessageReadStatus(messageId = message.id)
         }
     }
