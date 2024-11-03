e3edbd438a88644a62d9121cb65f58e950960ae4
정진호 <akgk155@gmail.com>
2024-11-03 21:48:11 +0900

    [CHORE]#190 : 삽입 시 패키지 이름 소거

--- app/src/main/kotlin/com/bff/wespot/MainActivity.kt
+++ app/src/main/kotlin/com/bff/wespot/MainActivity.kt
@@ -603,25 +603,25 @@ private enum class RestrictionContent(
     val buttonNumber: Int = 1,
 ) {
     TYPE1(
-        com.bff.wespot.R.string.restriction_title,
-        com.bff.wespot.R.string.restriction_type1_body1,
-        com.bff.wespot.R.string.restriction_type1_body2,
-        com.bff.wespot.R.string.restriction_type1_body3,
-        com.bff.wespot.R.string.restriction_type1_body4,
+        string.restriction_title,
+        string.restriction_type1_body1,
+        string.restriction_type1_body2,
+        string.restriction_type1_body3,
+        string.restriction_type1_body4,
     ),
 
     TYPE2(
-        com.bff.wespot.R.string.restriction_title,
-        com.bff.wespot.R.string.restriction_type1_body1,
-        com.bff.wespot.R.string.restriction_type2_body2,
-        com.bff.wespot.R.string.restriction_type2_body3,
+        string.restriction_title,
+        string.restriction_type1_body1,
+        string.restriction_type2_body2,
+        string.restriction_type2_body3,
     ),
 
     TYPE3(
-        com.bff.wespot.R.string.restriction_title,
-        com.bff.wespot.R.string.restriction_type1_body1,
-        com.bff.wespot.R.string.restriction_type3_body2,
-        com.bff.wespot.R.string.restriction_type3_body3,
+        string.restriction_title,
+        string.restriction_type1_body1,
+        string.restriction_type3_body2,
+        string.restriction_type3_body3,
         null,
         2,
     );
