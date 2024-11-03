4aa487b478f98c988eda0c57f1965c885406d90b
정진호 <akgk155@gmail.com>
2024-11-03 23:22:29 +0900

    [CHORE]#190 : formatting

--- feature/auth/src/main/kotlin/com/bff/wespot/auth/screen/SchoolScreen.kt
+++ feature/auth/src/main/kotlin/com/bff/wespot/auth/screen/SchoolScreen.kt
@@ -21,7 +21,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
