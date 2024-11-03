27d0bd62bdf322047875b6418cdf90a2a5ba5201
정진호 <akgk155@gmail.com>
2024-11-03 21:45:24 +0900

    [CHORE]#190 : 알림 아이콘 변경

--- feature/notification/src/main/kotlin/com/bff/wespot/notification/screen/NotificationScreen.kt
+++ feature/notification/src/main/kotlin/com/bff/wespot/notification/screen/NotificationScreen.kt
@@ -207,7 +207,13 @@ fun NotificationListItem(
         ) {
             Box {
                 Icon(
-                    painter = painterResource(id = R.drawable.ic_notification),
+                    painter = painterResource(
+                        id = if (notification.type.isVoteNotificationType()) {
+                            R.drawable.ic_vote_notification
+                        } else {
+                            R.drawable.ic_message_notification
+                        },
+                    ),
                     contentDescription = stringResource(
                         com.bff.wespot.designsystem.R.string.notification_icon,
                     ),
