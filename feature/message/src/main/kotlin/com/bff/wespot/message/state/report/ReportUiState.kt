08e2251f079c9e41f0b79eccc3e94a86376ef23e
정진호 <akgk155@gmail.com>
2024-11-05 23:41:50 +0900

    [CHORE]#193 : 리소스 수정 및 인덴트 수정

--- feature/message/src/main/kotlin/com/bff/wespot/message/state/report/ReportUiState.kt
+++ feature/message/src/main/kotlin/com/bff/wespot/message/state/report/ReportUiState.kt
@@ -2,7 +2,7 @@ package com.bff.wespot.message.state.report
 
 import com.bff.wespot.message.model.ReportReason
 
-data class ReportUiState (
+data class ReportUiState(
     val messageId: Int,
     val reportReason: ReportReason = ReportReason(),
     val inputReportReason: String = "",
