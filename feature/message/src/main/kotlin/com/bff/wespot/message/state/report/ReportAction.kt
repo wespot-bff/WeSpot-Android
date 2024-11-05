8f0cf8129729ff63c1339676956674e84355add9
정진호 <akgk155@gmail.com>
2024-11-05 21:49:32 +0900

    [FEATURE]#193 : 쪽지 신고 기능 구현

---
+++ feature/message/src/main/kotlin/com/bff/wespot/message/state/report/ReportAction.kt
@@ -0,0 +1,9 @@
+package com.bff.wespot.message.state.report
+
+import com.bff.wespot.message.model.ReportReason
+
+sealed class ReportAction {
+    data object OnMessageReportButtonClicked : ReportAction()
+    data class OnReportReasonSelected(val reportReason: ReportReason) : ReportAction()
+    data class OnReportReasonChanged(val reason: String) : ReportAction()
+}
