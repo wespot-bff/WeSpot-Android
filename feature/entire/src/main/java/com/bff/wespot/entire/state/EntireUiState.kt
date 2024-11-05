6f220002e5b28e619faae911bc69c6716551a058
정진호 <akgk155@gmail.com>
2024-11-05 23:41:25 +0900

    [FEATURE]#193 : 탈퇴 사유 기능 상태 구현

--- feature/entire/src/main/java/com/bff/wespot/entire/state/EntireUiState.kt
+++ feature/entire/src/main/java/com/bff/wespot/entire/state/EntireUiState.kt
@@ -9,8 +9,8 @@ import kotlinx.coroutines.flow.flow
 data class EntireUiState(
     val profile: Profile = Profile(),
     val revokeReasonList: List<String> = listOf(),
-    val inputReportReason: String = "",
-    val isInputReportReasonSelected: Boolean = false,
+    val inputRevokeReason: String = "",
+    val isInputRevokeReasonSelected: Boolean = false,
     val revokeConfirmed: Boolean = false,
     val blockedMessageList: Flow<PagingData<BlockedMessage>> = flow { },
     val unBlockList: List<Int> = listOf(),
--- feature/entire/src/main/java/com/bff/wespot/entire/viewmodel/EntireViewModel.kt
+++ feature/entire/src/main/java/com/bff/wespot/entire/viewmodel/EntireViewModel.kt
@@ -54,6 +54,7 @@ class EntireViewModel @Inject constructor(
             EntireAction.OnRevokeButtonClicked -> revokeUser()
             EntireAction.OnSignOutButtonClicked -> signOut()
             EntireAction.UnBlockMessage -> unblockMessage()
+            EntireAction.OnInputRevokeReasonSelected -> handleInputRevokeReasonSelected()
             is EntireAction.OnUnBlockButtonClicked -> handleUnBlockButtonClicked(action.messageId)
             is EntireAction.OnRevokeReasonSelected -> handleRevokeReasonSelected(action.reason)
             is EntireAction.OnRevokeReasonChanged -> handleRevokeReasonChanged(action.reason)
@@ -93,8 +94,8 @@ class EntireViewModel @Inject constructor(
     }
 
     private fun revokeUser() = intent {
-        val revokeReason = if (state.isInputReportReasonSelected) {
-            state.revokeReasonList + state.inputReportReason
+        val revokeReason = if (state.isInputRevokeReasonSelected) {
+            state.revokeReasonList + state.inputRevokeReason
         } else {
             state.revokeReasonList
         }
@@ -166,30 +167,27 @@ class EntireViewModel @Inject constructor(
 
     private fun handleRevokeReasonSelected(reason: String) = intent {
         reduce {
-            /**
-             * 아이템이 선택되었는지 판단하는 기준
-             * 유저 입력 아이템인 경우, 따로 Boolean Flag를 통해 관리한다.
-             * 그외 아이템의 경우, 동적 리스트를 통해 관리한다.
-             */
-            if (reason == "직접 입력") {
-                state.copy(isInputReportReasonSelected = state.isInputReportReasonSelected.not())
-            } else {
-                val updatedList = state.revokeReasonList.toMutableList().apply {
-                    if (contains(reason)) {
-                        remove(reason)
-                    } else {
-                        add(reason)
-                    }
+            val updatedList = state.revokeReasonList.toMutableList().apply {
+                if (contains(reason)) {
+                    remove(reason)
+                } else {
+                    add(reason)
                 }
-                state.copy(revokeReasonList = updatedList)
             }
+            state.copy(revokeReasonList = updatedList)
+        }
+    }
+
+    private fun handleInputRevokeReasonSelected() = intent {
+        reduce {
+            state.copy(isInputRevokeReasonSelected = state.isInputRevokeReasonSelected.not())
         }
     }
 
     private fun handleRevokeReasonChanged(reason: String) = intent {
         if (reason.length <= 100) {
             reduce {
-                state.copy(inputReportReason = reason)
+                state.copy(inputRevokeReason = reason)
             }
         }
     }
