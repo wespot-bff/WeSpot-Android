b802694c615dcb0c30f160f3c79acd66ca85485b
jeongjaino <akgk155@gmail.com>
2024-11-07 23:20:09 +0900

    [CHORE]#193 : Banner 하단 패딩 값 수정

--- feature/message/src/main/kotlin/com/bff/wespot/message/component/ReservedMessageBanner.kt
+++ feature/message/src/main/kotlin/com/bff/wespot/message/component/ReservedMessageBanner.kt
@@ -2,6 +2,7 @@ package com.bff.wespot.message.component
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.slideInVertically
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
@@ -20,34 +21,35 @@ internal fun ReservedMessageBanner(
     onBannerClick: () -> Unit,
 ) {
     AnimatedVisibility(
-        modifier = Modifier.padding(paddingValues),
         visible = messageStatus.hasReservedMessages() && messageStatus.countRemainingMessages >= 0,
         enter = slideInVertically { initialOffsetY -> -initialOffsetY },
     ) {
-        if (messageStatus.hasRemainingMessages()) {
-            WSBanner(
-                title = stringResource(
-                    R.string.reserved_message_banner_title,
-                    messageStatus.getReservedMessageCount(),
-                ),
-                subTitle = stringResource(
-                    R.string.reserved_message_banner_subtitle,
-                    messageStatus.countRemainingMessages,
-                ),
-                image = painterResource(id = R.drawable.reserved_message),
-                onBannerClick = { onBannerClick() },
-                bannerType = WSBannerType.Primary,
-            )
-        } else if (messageStatus.hasNoRemainingMessages()) {
-            WSBanner(
-                title = stringResource(
-                    R.string.reserved_message_banner_title_disabled,
-                    messageStatus.getReservedMessageCount(),
-                ),
-                image = painterResource(id = R.drawable.reserved_message),
-                onBannerClick = { onBannerClick() },
-                bannerType = WSBannerType.Secondary,
-            )
+        Box(modifier = Modifier.padding(paddingValues)) {
+            if (messageStatus.hasRemainingMessages()) {
+                WSBanner(
+                    title = stringResource(
+                        R.string.reserved_message_banner_title,
+                        messageStatus.getReservedMessageCount(),
+                    ),
+                    subTitle = stringResource(
+                        R.string.reserved_message_banner_subtitle,
+                        messageStatus.countRemainingMessages,
+                    ),
+                    image = painterResource(id = R.drawable.reserved_message),
+                    onBannerClick = { onBannerClick() },
+                    bannerType = WSBannerType.Primary,
+                )
+            } else if (messageStatus.hasNoRemainingMessages()) {
+                WSBanner(
+                    title = stringResource(
+                        R.string.reserved_message_banner_title_disabled,
+                        messageStatus.getReservedMessageCount(),
+                    ),
+                    image = painterResource(id = R.drawable.reserved_message),
+                    onBannerClick = { onBannerClick() },
+                    bannerType = WSBannerType.Secondary,
+                )
+            }
         }
     }
 }
