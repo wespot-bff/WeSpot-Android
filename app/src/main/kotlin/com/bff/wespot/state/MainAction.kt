20e1deb64b69d9c0eaf97fb3b3b2edc26dd7155b
jeongjaino <akgk155@gmail.com>
2024-12-01 21:31:17 +0900

    [FEATURE]#211 : VersionUpdateDialogState 구현

--- app/src/main/kotlin/com/bff/wespot/state/MainAction.kt
+++ app/src/main/kotlin/com/bff/wespot/state/MainAction.kt
@@ -3,8 +3,8 @@ package com.bff.wespot.state
 import com.bff.wespot.MainScreenNavArgs
 
 sealed class MainAction {
-    data object OnMainScreenEntered : MainAction()
     data object OnNavigateByPushNotification : MainAction()
+    data class OnMainScreenEntered(val appVersionName: String) : MainAction()
     data class OnEnteredByPushNotification(val data: MainScreenNavArgs) : MainAction()
     data class OnNotificationSet(val isEnableNotification: Boolean) : MainAction()
 }
