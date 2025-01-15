27730e75c0d1923312ad37b9bc3860771df78f93
jeongjaino <akgk155@gmail.com>
2025-01-15 20:45:01 +0900

    [REFACTOR]#220 : MessageReportScreen -> Dialog로 변경

--- feature/message/src/main/kotlin/com/bff/wespot/message/state/report/ReportAction.kt
+++ feature/message/src/main/kotlin/com/bff/wespot/message/state/report/ReportAction.kt
@@ -4,6 +4,7 @@ import com.bff.wespot.message.model.ReportReason
 
 sealed class ReportAction {
     data object OnMessageReportButtonClicked : ReportAction()
+    data class OnMessageReportScreenEntered(val messageId: Int) : ReportAction()
     data class OnReportReasonSelected(val reportReason: ReportReason) : ReportAction()
     data class OnReportReasonChanged(val reason: String) : ReportAction()
 }
