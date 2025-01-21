e225abf586bed2d1ba39ce6f22432261a6d241a8
jeongjaino <akgk155@gmail.com>
2025-01-21 23:32:50 +0900

    [CHORE]#217 : 미사용 변수  minVersion 제거

--- app/src/main/kotlin/com/bff/wespot/splash/SplashViewModel.kt
+++ app/src/main/kotlin/com/bff/wespot/splash/SplashViewModel.kt
@@ -3,7 +3,6 @@ package com.bff.wespot.splash
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.bff.wespot.domain.repository.firebase.config.RemoteConfigRepository
-import com.bff.wespot.domain.util.RemoteConfigKey
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -12,20 +11,14 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SplashViewModel @Inject constructor(
-    private val dataSource: RemoteConfigRepository
+    private val remoteConfigRepository: RemoteConfigRepository,
 ) : ViewModel() {
     private val _start = MutableStateFlow(false)
     val start = _start.asStateFlow()
 
-    private val _minVersion = MutableStateFlow("")
-    val minVersion = _minVersion.asStateFlow()
-
     init {
         viewModelScope.launch {
-            _start.value = dataSource.startRemoteConfig()
-            if (_start.value) {
-                _minVersion.emit(dataSource.fetchFromRemoteConfig(RemoteConfigKey.MIN_VERSION))
-            }
+            _start.value = remoteConfigRepository.startRemoteConfig()
         }
     }
 }
