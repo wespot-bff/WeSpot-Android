ff704582a87696907aa2a39cf510f170a69d7823
jeongjaino <akgk155@gmail.com>
2025-02-01 00:28:34 +0900

    [CHORE]#218 : ImageComponent 내부 함수 소거

--- core/model/src/main/kotlin/com/bff/wespot/model/dynamicui/component/ImageComponent.kt
+++ core/model/src/main/kotlin/com/bff/wespot/model/dynamicui/component/ImageComponent.kt
@@ -4,7 +4,4 @@ data class ImageComponent(
     val url: String = "",
     val width: Int = -1,
     val height: Int = -1,
-) : DynamicUiComponent {
-    fun isFillMaxWidth() = this.width == 0
-    fun isFillMaxHeight() = this.height == 0
-}
+) : DynamicUiComponent
