dbbf12a93bee3a30a95047939d6c91cc7bd16781
jeongjaino <akgk155@gmail.com>
2025-03-03 21:36:29 +0900

    [CHORE]#237 : 프로필 이미지 Crop으로 설정

--- feature/entire/src/main/java/com/bff/wespot/entire/screen/edit/ProfileEditScreen.kt
+++ feature/entire/src/main/java/com/bff/wespot/entire/screen/edit/ProfileEditScreen.kt
@@ -37,6 +37,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalView
@@ -186,6 +187,7 @@ fun ProfileEditScreen(
                         contentDescription = stringResource(
                             com.bff.wespot.ui.R.string.user_character_image,
                         ),
+                        contentScale = ContentScale.Crop,
                     )
 
                     Box(
