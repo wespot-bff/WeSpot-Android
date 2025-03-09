3de98e23a4e03c2a245f680ff3f8570dc54bbddd
jeongjaino <akgk155@gmail.com>
2025-03-09 16:13:04 +0900

    [FEATURE]#240 : MessageStorageRepository에서 Flow 수집 및 메세지 DB처리 구문 추가해요

--- domain/src/main/kotlin/com/bff/wespot/domain/repository/message/MessageStorageRepository.kt
+++ domain/src/main/kotlin/com/bff/wespot/domain/repository/message/MessageStorageRepository.kt
@@ -1,8 +1,13 @@
 package com.bff.wespot.domain.repository.message
 
+import androidx.paging.PagingData
 import com.bff.wespot.model.message.response.Message
+import com.bff.wespot.model.message.response.ReceivedMessage
+import kotlinx.coroutines.flow.Flow
 
 interface MessageStorageRepository {
+    fun fetchReceivedMessageStream(): Flow<PagingData<ReceivedMessage>>
+
     suspend fun updateMessageReadStatus(messageId: Int): Result<Unit>
 
     suspend fun deleteMessage(messageId: Int): Result<Unit>
