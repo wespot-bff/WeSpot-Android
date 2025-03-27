3f1fa4451a366c3ca28871c86e08d9213a14a645
jeongjaino <akgk155@gmail.com>
2025-03-27 23:41:08 +0900

    [FEAT]#244 : ProfileCircleImage 공용 컴포넌트 구현

---
+++ core/ui/src/main/kotlin/com/bff/wespot/ui/component/ProfileCircleImage.kt
@@ -0,0 +1,49 @@
+package com.bff.wespot.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.Dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.bff.wespot.designsystem.theme.WeSpotThemeManager
+import com.bff.wespot.ui.util.hexToColor
+
+@Composable
+fun ProfileCircleImage(
+    size: Dp,
+    imageUrl: String,
+    backgroundColor: String,
+    contentDescription: String,
+) {
+    Box(
+        modifier = Modifier
+            .size(size)
+            .clip(CircleShape)
+            .background(
+                if (backgroundColor.isNotEmpty()) {
+                    hexToColor(backgroundColor)
+                } else {
+                    WeSpotThemeManager.colors.cardBackgroundColor
+                },
+            ),
+        contentAlignment = Alignment.Center,
+    ) {
+        AsyncImage(
+            modifier = Modifier.size(size),
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(imageUrl)
+                .crossfade(true)
+                .build(),
+            contentDescription = contentDescription,
+            contentScale = ContentScale.Crop,
+        )
+    }
+}
