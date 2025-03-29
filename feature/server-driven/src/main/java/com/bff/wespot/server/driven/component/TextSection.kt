b27da1fcccf842af19c3b09c1de220acf2fbca72
flash159483 <seungwonjung0313@gmail.com>
2025-03-29 17:57:59 +0900

    [feat]#232: padding 공통화 추가

--- feature/server-driven/src/main/java/com/bff/wespot/server/driven/component/TextSection.kt
+++ feature/server-driven/src/main/java/com/bff/wespot/server/driven/component/TextSection.kt
@@ -4,12 +4,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.bff.wespot.model.serverDriven.Paddings
 import com.bff.wespot.model.serverDriven.RichText
 import com.bff.wespot.server.driven.util.toColor
 import com.bff.wespot.server.driven.util.toFontWeight
+import com.bff.wespot.server.driven.util.toPaddingValues
 import com.bff.wespot.server.driven.util.toTextAlign
 
 @Composable
@@ -23,12 +23,7 @@ internal fun TextSection(
         fontSize = richText.fontSize.sp,
         fontWeight = richText.fontWeight.toFontWeight(),
         modifier = modifier
-            .padding(
-                start = paddings.start.dp,
-                top = paddings.top.dp,
-                end = paddings.end.dp,
-                bottom = paddings.bottom.dp,
-            ),
+            .padding(paddings.toPaddingValues()),
         textAlign = richText.align.toTextAlign(),
         color = richText.color.toColor(),
     )
