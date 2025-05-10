b6c7c4b0c731ad689d6f978cc59651fa432d2296
jeongjaino <akgk155@gmail.com>
2025-05-10 14:43:39 +0900

    [FEATURE]#245 : MessageListDto Serialization 태그 추가

--- data-remote/src/main/kotlin/com/bff/wespot/data/remote/model/message/response/MessageListDto.kt
+++ data-remote/src/main/kotlin/com/bff/wespot/data/remote/model/message/response/MessageListDto.kt
@@ -1,5 +1,8 @@
 package com.bff.wespot.data.remote.model.message.response
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class MessageListDto(
     val messages: List<MessageDto>,
     val lastCursorId: Int,
