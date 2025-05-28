cd8537d7b0e239a7e97c9476c481c7185e7e5ca4
jeongjaino <akgk155@gmail.com>
2025-05-29 00:50:05 +0900

    [FEATURE] #245 : 쪽지 아이템 UI 변경, 쪽지 차단 기능 구현

--- feature/message/src/main/kotlin/com/bff/wespot/message/state/storage/StorageAction.kt
+++ feature/message/src/main/kotlin/com/bff/wespot/message/state/storage/StorageAction.kt
@@ -1,19 +1,15 @@
 package com.bff.wespot.message.state.storage
 
-import com.bff.wespot.message.model.MessageOptionType
 import com.bff.wespot.model.message.response.Message
 
 sealed class StorageAction {
-    data object OnMessageBlockButtonClicked : StorageAction()
-    data object OnMessageDeleteButtonClicked : StorageAction()
-    data object OnMessageReportButtonClicked : StorageAction()
     data class OnStorageChipSelected(val screenIndex: Int) : StorageAction()
     data class OnMessageClicked(val message: Message) : StorageAction()
-    data class OnOptionButtonClicked(val messageId: Int) : StorageAction()
-    data class OnBookmarkButtonClicked(val messageId: Int) : StorageAction()
-    data class OnOptionBottomSheetClicked(val messageOptionType: MessageOptionType) : StorageAction()
+    data class OnOptionButtonClicked(val message: Message) : StorageAction()
     data class OnPushNotificationNavigated(val messageId: Int) : StorageAction()
-    data object OnOptionDialogClosed : StorageAction()
+    data object OnBlockBottomSheetItemClicked : StorageAction()
+    data object OnBookmarkBottomSheetItemClicked : StorageAction()
     data object OnOptionBottomSheetClosed : StorageAction()
-    data object OnMessageReportScreenClosed : StorageAction()
+    data object OnBlockButtonClicked : StorageAction()
+    data object OnBlockDialogClosed : StorageAction()
 }
