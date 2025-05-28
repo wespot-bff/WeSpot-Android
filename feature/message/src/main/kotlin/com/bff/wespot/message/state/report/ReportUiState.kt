3aeef406a671372ebd96708313023307ab740cdc
jeongjaino <akgk155@gmail.com>
2025-05-29 00:34:06 +0900

    [CHORE] #245 : 쪽지 신고 관련 속성 제거

--- feature/message/src/main/kotlin/com/bff/wespot/message/state/report/ReportUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.bff.wespot.message.state.report
-
-import com.bff.wespot.message.model.ReportReason
-
-data class ReportUiState(
-    val messageId: Int = -1,
-    val reportReason: ReportReason = ReportReason(),
-    val inputReportReason: String = "",
-)
