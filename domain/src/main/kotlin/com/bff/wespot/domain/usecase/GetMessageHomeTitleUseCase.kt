274a6149c743e6d7a62ada3df17c397477d31574
jeongjaino <akgk155@gmail.com>
2025-06-01 00:55:01 +0900

    [CHORE] #243 : 새로운 CoroutineScope -> viewModelScope 내 coroutineScope 사용하도록 수정

--- domain/src/main/kotlin/com/bff/wespot/domain/usecase/GetMessageHomeTitleUseCase.kt
+++ domain/src/main/kotlin/com/bff/wespot/domain/usecase/GetMessageHomeTitleUseCase.kt
@@ -4,49 +4,48 @@ import com.bff.wespot.domain.repository.DataStoreRepository
 import com.bff.wespot.domain.repository.message.MessageRepository
 import com.bff.wespot.domain.repository.user.ProfileRepository
 import com.bff.wespot.domain.util.DataStoreKey
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class GetMessageHomeTitleUseCase @Inject constructor(
-    private val dispatcher: CoroutineDispatcher,
     private val messageRepository: MessageRepository,
     private val dataStoreRepository: DataStoreRepository,
     private val profileRepository: ProfileRepository,
 ) {
-    suspend operator fun invoke(): String {
-        checkAndCacheMessageHomeTitle()
+    suspend operator fun invoke(): String = coroutineScope {
+        launch(SupervisorJob()) {
+            checkAndCacheMessageHomeTitle()
+        }
 
         val title = dataStoreRepository.getString(DataStoreKey.MESSAGE_HOME_TITLE).firstOrNull()
-        return if (title.isNullOrEmpty()) {
+        if (title.isNullOrEmpty()) {
             getDefaultTitle()
         } else {
             title
         }
     }
 
-    private fun checkAndCacheMessageHomeTitle() {
-        CoroutineScope(dispatcher).launch {
-            val cachedTimeString = dataStoreRepository.getString(
-                DataStoreKey.MESSAGE_HOME_TITLE_CACHED_TIME,
-            ).firstOrNull()
-            val cachedTime = cachedTimeString?.toLongOrNull() ?: 0L
-            val currentTime = System.currentTimeMillis()
+    private suspend fun checkAndCacheMessageHomeTitle() {
+        val cachedTimeString = dataStoreRepository.getString(
+            DataStoreKey.MESSAGE_HOME_TITLE_CACHED_TIME,
+        ).firstOrNull()
+        val cachedTime = cachedTimeString?.toLongOrNull() ?: 0L
+        val currentTime = System.currentTimeMillis()
 
-            /** 아직 캐싱 유효 시간이 지나지 않았다면 바로 반환한다. */
-            if ((currentTime - cachedTime) <= CACHE_EXPIRATION_TIME_MILLIS) return@launch
+        /** 아직 캐싱 유효 시간이 지나지 않았다면 바로 반환한다. */
+        if ((currentTime - cachedTime) <= CACHE_EXPIRATION_TIME_MILLIS) return
 
-            messageRepository.getMessageHomeTitle()
-                .onSuccess {
-                    dataStoreRepository.saveString(DataStoreKey.MESSAGE_HOME_TITLE, it.title)
-                    dataStoreRepository.saveString(
-                        DataStoreKey.MESSAGE_HOME_TITLE_CACHED_TIME,
-                        currentTime.toString(),
-                    )
-                }
-        }
+        messageRepository.getMessageHomeTitle()
+            .onSuccess {
+                dataStoreRepository.saveString(DataStoreKey.MESSAGE_HOME_TITLE, it.title)
+                dataStoreRepository.saveString(
+                    DataStoreKey.MESSAGE_HOME_TITLE_CACHED_TIME,
+                    currentTime.toString(),
+                )
+            }
     }
 
     private suspend fun getDefaultTitle(): String {
