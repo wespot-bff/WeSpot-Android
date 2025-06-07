442b77283e40e399de5ccab5d661af43474c5009
jeongjaino <akgk155@gmail.com>
2025-06-07 19:18:49 +0900

    [CHORE] #247 : SideEffect 사용하지 않는 NoneSideEffect 구현

--- feature/message/src/main/kotlin/com/bff/wespot/message/viewmodel/MessageNotificationSettingViewModel.kt
+++ feature/message/src/main/kotlin/com/bff/wespot/message/viewmodel/MessageNotificationSettingViewModel.kt
@@ -4,9 +4,9 @@ import androidx.lifecycle.viewModelScope
 import com.bff.wespot.common.extension.onNetworkFailure
 import com.bff.wespot.domain.repository.user.UserRepository
 import com.bff.wespot.message.state.setting.MessageNotificationSettingAction
-import com.bff.wespot.message.state.setting.MessageNotificationSettingSideEffect
 import com.bff.wespot.message.state.setting.MessageNotificationSettingUiState
 import com.bff.wespot.ui.base.BaseViewModel
+import com.bff.wespot.ui.base.NoneSideEffect
 import com.bff.wespot.ui.model.SideEffect.Companion.toSideEffect
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -21,8 +21,8 @@ import javax.inject.Inject
 @HiltViewModel
 class MessageNotificationSettingViewModel @Inject constructor(
     private val repository: UserRepository,
-) : BaseViewModel(), ContainerHost<MessageNotificationSettingUiState, MessageNotificationSettingSideEffect> {
-    override val container: Container<MessageNotificationSettingUiState, MessageNotificationSettingSideEffect> =
+) : BaseViewModel(), ContainerHost<MessageNotificationSettingUiState, NoneSideEffect> {
+    override val container: Container<MessageNotificationSettingUiState, NoneSideEffect> =
         container(MessageNotificationSettingUiState()) {
             getNotificationSetting()
         }
