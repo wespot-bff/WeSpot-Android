5f8dfecc10fa2b764b64d5ac46f2044122a7fcef
jeongjaino <akgk155@gmail.com>
2025-06-08 19:10:10 +0900

    [FEATURE] #259 : 바텀 시트 내 좌/우 패딩 20 -> 32 변경, 바텀 시트 TextList Vertical Center로 변경

--- feature/server-driven/src/main/java/com/bff/wespot/server/driven/component/TextListSection.kt
+++ feature/server-driven/src/main/java/com/bff/wespot/server/driven/component/TextListSection.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -29,6 +30,7 @@ internal fun TextListSection(
             val item = textList[it]
             Row(
                 modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.spacedBy(16.dp),
             ) {
                 AsyncImage(
