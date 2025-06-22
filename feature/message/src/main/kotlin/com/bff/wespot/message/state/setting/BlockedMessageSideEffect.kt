7bc632c23ffb147437f69694f6c51bef231499bf
jeongjaino <akgk155@gmail.com>
2025-06-22 17:10:05 +0900

    [FEATURE] #247 : 쪽지 차단 시, 확인 모달 노출하도록 설정해요

--- feature/message/src/main/kotlin/com/bff/wespot/message/state/setting/BlockedMessageSideEffect.kt
+++ feature/message/src/main/kotlin/com/bff/wespot/message/state/setting/BlockedMessageSideEffect.kt
@@ -3,5 +3,7 @@ package com.bff.wespot.message.state.setting
 import com.bff.wespot.ui.model.ToastState
 
 sealed interface BlockedMessageSideEffect {
+    data object ShowDialog : BlockedMessageSideEffect
+    data object DismissDialog : BlockedMessageSideEffect
     data class ShowToast(val toastState: ToastState) : BlockedMessageSideEffect
 }
--- feature/message/src/main/kotlin/com/bff/wespot/message/state/setting/BlockedMessageUiState.kt
+++ feature/message/src/main/kotlin/com/bff/wespot/message/state/setting/BlockedMessageUiState.kt
@@ -4,5 +4,6 @@ import com.bff.wespot.model.message.response.Message
 
 data class BlockedMessageUiState(
     val isLoading: Boolean = false,
+    val messageId: Int = -1,
     val messageList: List<Message> = listOf(),
 )
