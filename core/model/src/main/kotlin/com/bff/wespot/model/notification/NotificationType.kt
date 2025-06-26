48123c1cd8fff24f985b6f66e21298e120ac3214
jeongjaino <akgk155@gmail.com>
2025-06-27 00:01:55 +0900

    [FEATURE]#251 : MessageV2 타입 구현

--- core/model/src/main/kotlin/com/bff/wespot/model/notification/NotificationType.kt
+++ core/model/src/main/kotlin/com/bff/wespot/model/notification/NotificationType.kt
@@ -3,8 +3,7 @@ package com.bff.wespot.model.notification
 enum class NotificationType {
     IDLE,
     MESSAGE,
-    MESSAGE_SENT,
-    MESSAGE_RECEIVED,
+    MESSAGE_V2,
     VOTE,
     VOTE_RESULT,
     VOTE_RECEIVED,
@@ -12,7 +11,7 @@ enum class NotificationType {
     ;
 
     fun toDescription(): String = when (this) {
-        MESSAGE, MESSAGE_SENT, MESSAGE_RECEIVED -> "쪽지 알림"
+        MESSAGE, MESSAGE_V2 -> "쪽지 알림"
         VOTE, VOTE_RESULT, VOTE_RECEIVED -> "투표 알림"
         PROFILE_UPDATE -> "프로필 알림"
         IDLE -> ""
@@ -21,16 +20,8 @@ enum class NotificationType {
     fun isVoteNotificationType(): Boolean = this in listOf(VOTE, VOTE_RESULT, VOTE_RECEIVED)
 
     companion object {
-        fun convertNotificationType(type: String): NotificationType =
-            when (type) {
-                MESSAGE.name -> MESSAGE
-                MESSAGE_SENT.name -> MESSAGE_SENT
-                MESSAGE_RECEIVED.name -> MESSAGE_RECEIVED
-                VOTE.name -> VOTE
-                VOTE_RESULT.name -> VOTE_RESULT
-                VOTE_RECEIVED.name -> VOTE_RECEIVED
-                PROFILE_UPDATE.name -> PROFILE_UPDATE
-                else -> IDLE
-            }
+        fun convertNotificationType(type: String): NotificationType = runCatching {
+            NotificationType.valueOf(type)
+        }.getOrDefault(IDLE)
     }
 }
