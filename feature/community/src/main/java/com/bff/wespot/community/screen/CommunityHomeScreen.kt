398614b7f276ac270fe82615c58dbc62671baa60
flash159483 <seungwonjung0313@gmail.com>
2025-07-21 22:04:56 +0900

    [feat]#268: 커뮤니티 모듈 추

---
+++ feature/community/src/main/java/com/bff/wespot/community/screen/CommunityHomeScreen.kt
@@ -0,0 +1,12 @@
+package com.bff.wespot.community.screen
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Destination
+@Composable
+internal fun CommunityHomeScreen(
+
+) {
+
+}
