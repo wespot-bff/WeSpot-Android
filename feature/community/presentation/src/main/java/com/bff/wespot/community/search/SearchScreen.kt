e4ff7b0c7386ed242b5fedd78268bfe74688dd80
flash159483 <seungwonjung0313@gmail.com>
2025-07-25 21:31:13 +0900

    [feat]#278: UI 추가

---
+++ feature/community/presentation/src/main/java/com/bff/wespot/community/search/SearchScreen.kt
@@ -0,0 +1,74 @@
+package com.bff.wespot.community.search
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.compose.itemKey
+import com.bff.wespot.community.component.Item
+import com.bff.wespot.community.search.state.SearchAction
+import com.bff.wespot.community.search.state.SearchUiState
+import com.bff.wespot.community.uimodel.PostItemUiModel
+import com.bff.wespot.designsystem.component.header.WSTopBar
+import com.bff.wespot.designsystem.component.input.WsTextField
+import com.bff.wespot.designsystem.component.input.WsTextFieldType
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
+@Composable
+internal fun SearchScreen(
+    navigateUp: () -> Unit,
+    action: (SearchAction) -> Unit,
+    state: SearchUiState,
+) {
+    val pagingData = state.searches.collectAsLazyPagingItems()
+
+    Scaffold(
+        topBar = {
+            WSTopBar(
+                title = "",
+                canNavigateBack = true,
+                navigateUp = navigateUp,
+            )
+        },
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .padding(it)
+                .padding(start = 20.dp, end = 20.dp, top = 12.dp),
+        ) {
+            stickyHeader {
+                WsTextField(
+                    value = state.keyword,
+                    onValueChange = {
+                        action(SearchAction.HandleSearchChange(it))
+                    },
+                    placeholder = "글 제목 내용을 검색해 주세요",
+                    textFieldType = WsTextFieldType.Search,
+                )
+            }
+
+            items(
+                key = pagingData.itemKey(),
+                count = pagingData.itemCount,
+            ) {
+                when (val post = pagingData[it]) {
+                    is PostItemUiModel -> {
+                        post.content.Item(
+                            navigateToPost = {
+                                action(SearchAction.NavigateToDetail(post.id))
+                            },
+                        )
+                    }
+                    else -> {
+                        // 지원할 필요 없음
+                    }
+                }
+            }
+        }
+    }
+}
