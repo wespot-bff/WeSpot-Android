508b7cfe68e5a0a7a3dedb92f9690b48480561be
flash159483 <seungwonjung0313@gmail.com>
2025-07-28 18:14:08 +0900

    [feat]#280: 링킹 처리를 추가해요

--- feature/community/presentation/src/main/java/com/bff/wespot/community/all/CommunityAllActivity.kt
+++ feature/community/presentation/src/main/java/com/bff/wespot/community/all/CommunityAllActivity.kt
@@ -8,6 +8,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
@@ -17,6 +20,7 @@ import com.bff.wespot.community.all.screen.AllPostsScreen
 import com.bff.wespot.community.all.screen.CommunityAllScreen
 import com.bff.wespot.community.all.screen.MyCommentsScreen
 import com.bff.wespot.community.all.screen.ScrapsScreen
+import com.bff.wespot.community.presentation.R
 import com.bff.wespot.designsystem.component.header.WSTopBar
 import com.bff.wespot.designsystem.theme.WeSpotTheme
 import com.bff.wespot.navigation.Navigator
@@ -36,12 +40,15 @@ class CommunityAllActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             val navController = rememberNavController()
+            var title by remember {
+                mutableStateOf("")
+            }
 
             WeSpotTheme {
                 Scaffold(
                     topBar = {
                         WSTopBar(
-                            title = "",
+                            title = title,
                             canNavigateBack = true,
                             navigateUp = ::finish,
                         )
@@ -58,27 +65,36 @@ class CommunityAllActivity : ComponentActivity() {
 
                             CommunityAllScreen(
                                 onNavigateToAllPosts = {
+                                    title = getString(R.string.all_post_written)
                                     navController.navigate("all_posts")
                                 },
                                 onNavigateToMyComments = {
+                                    title = getString(R.string.all_comment_written)
                                     navController.navigate("my_comments")
                                 },
                                 onNavigateToScraps = {
+                                    title = getString(R.string.all_post_scraped)
                                     navController.navigate("scraps")
                                 },
                             )
                         }
 
                         composable("all_posts") {
-                            AllPostsScreen()
+                            AllPostsScreen(
+                                navigateUp = navController::navigateUp,
+                            )
                         }
 
                         composable("my_comments") {
-                            MyCommentsScreen()
+                            MyCommentsScreen(
+                                navigateUp = navController::navigateUp,
+                            )
                         }
 
                         composable("scraps") {
-                            ScrapsScreen()
+                            ScrapsScreen(
+                                navigateUp = navController::navigateUp,
+                            )
                         }
                     }
                 }
