65ca9bd426c123612127b670beb586e1f5777fa0
flash159483 <seungwonjung0313@gmail.com>
2025-08-04 21:23:00 +0900

    [feat]#271: scope 한개 쓰도록 수정

--- feature/community/presentation/src/main/java/com/bff/wespot/community/detail/PostDetailViewModel.kt
+++ feature/community/presentation/src/main/java/com/bff/wespot/community/detail/PostDetailViewModel.kt
@@ -29,31 +29,33 @@ class PostDetailViewModel @Inject constructor(
 
     init {
         viewModelScope.launch(coroutineDispatcher) {
-            postDetailRepository.getPostDetail(postId = param.postId)
-                .onNetworkFailure {
-                    postSideEffect(it.toSideEffect())
-                }
-                .onSuccess {
-                    intent {
-                        reduce {
-                            state.copy(detail = it.toUiModel())
+            launch {
+                postDetailRepository.getPostDetail(postId = param.postId)
+                    .onNetworkFailure {
+                        postSideEffect(it.toSideEffect())
+                    }
+                    .onSuccess {
+                        intent {
+                            reduce {
+                                state.copy(detail = it.toUiModel())
+                            }
                         }
                     }
-                }
-        }
+            }
 
-        viewModelScope.launch(coroutineDispatcher) {
-            postDetailRepository.getPostComments(param.postId)
-                .onNetworkFailure {
-                    postSideEffect(it.toSideEffect())
-                }
-                .onSuccess {
-                    intent {
-                        reduce {
-                            state.copy(comments = it.map { it.toUiModel() })
+            launch {
+                postDetailRepository.getPostComments(param.postId)
+                    .onNetworkFailure {
+                        postSideEffect(it.toSideEffect())
+                    }
+                    .onSuccess {
+                        intent {
+                            reduce {
+                                state.copy(comments = it.map { it.toUiModel() })
+                            }
                         }
                     }
-                }
+            }
         }
     }
 }
