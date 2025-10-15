3344939eab1090477ef62013ba5d4b8154231e4c
jeongjaino <akgk155@gmail.com>
2025-10-15 23:54:19 +0900

    [FEATURE]#296 : 바텀 네비게이션 높이 설정 69Dp

--- app/src/main/kotlin/com/bff/wespot/main/component/BottomNavigationTab.kt
+++ app/src/main/kotlin/com/bff/wespot/main/component/BottomNavigationTab.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
@@ -39,7 +40,7 @@ internal fun BottomNavigationTab(
 ) {
     NavigationBar(
         containerColor = WeSpotThemeManager.colors.naviColor,
-        modifier = modifier,
+        modifier = modifier.height(69.dp),
     ) {
         Row(
             modifier = Modifier.fillMaxWidth(),
